ddd29f4e72d7f159794c6bc8230edfd434b72845
Junseo Park <wonderbotsupport@naver.com>
2020-09-06 13:03:17 +0900

    Update migrate.sql

--- migrate.sql
+++ migrate.sql
@@ -10,3 +10,33 @@ UPDATE `bots` SET discord=NULL where discord='false';
 UPDATE `bots` SET vanity=NULL where vanity='false';
 UPDATE `bots` SET bg=NULL where bg='false';
 UPDATE `bots` SET banner=NULL where banner='false'
+
+-- ALTER submitted TABLE
+ALTER TABLE `submitted` CHANGE `servers` `servers` INT(11) NULL DEFAULT NULL, CHANGE `web` `web` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `git` `git` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `url` `url` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `category` `category` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '\'[]\'', CHANGE `status` `status` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '\'???\'', CHANGE `name` `name` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `avatar` `avatar` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `verified` `verified` TINYINT(1) NULL DEFAULT '0', CHANGE `trusted` `trusted` TINYINT(1) NULL DEFAULT '0', CHANGE `discord` `discord` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+
+-- CREATE submits TABLE
+
+CREATE TABLE `submits` (
+  `id` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` int(11) NOT NULL,
+  `owners` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lib` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prefix` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `votes` int(11) NOT NULL DEFAULT 0,
+  `servers` int(11) DEFAULT NULL,
+  `intro` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `desc` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `web` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `git` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `url` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `category` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT '\'[]\'',
+  `status` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '\'???\'',
+  `name` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tag` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `verified` tinyint(1) DEFAULT 0,
+  `trusted` tinyint(1) DEFAULT 0,
+  `discord` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `state` int(1) NOT NULL DEFAULT 0,
+  `reason` tinytext COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci

5a256147e2a0e2812e4c5ddaff28e0b386267081
원더 <wonderbotsupport@naver.com>
2021-01-04 20:10:03 +0900

    feat: added Container component

---
+++ components/Container.tsx
@@ -0,0 +1,14 @@
+const Container = ({ className, children }:ContainerProps): JSX.Element => {
+	return (
+		<div className={`container mx-auto px-4 ${className}`}>
+			{children}
+		</div>
+	)
+}
+
+interface ContainerProps {
+  className?: string
+  children: JSX.Element
+}
+
+export default Container

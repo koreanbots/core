0ce0e2095b8d15713afda45533410f0dedd226b5
원더 <wonderbotsupport@naver.com>
2021-01-04 20:13:39 +0900

    feat: added color presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	purge: [],
+	darkMode: 'class', // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				'koreanbots-blue': '#3366FF',
+				'koreanbots-black': '#2C2F33',
+				'koreanbots-green': '#21BA45',
+				'koreanbots-red': '#DB2828',
+				'discord-blurple': '#7289DA',
+				'discord-dark': '#2C2F33',
+				'discord-black': '#23272A',
+				'discord-pink': '#FF73FA'
+			}
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }

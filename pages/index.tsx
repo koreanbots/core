be9fc5a91801a3d18e1a151e781232b72b6a0449
원더 <wonderbotsupport@naver.com>
2021-01-04 22:09:16 +0900

    feat: waves~~

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import { NextPage } from 'next'
 import Container from '../components/Container'
+import Wave from '../components/Wave'
 
 const Index: NextPage = () => {
 	return <>
-		<div className='bg-discord-blurple dark:bg-discord-dark'>
-			<Container className='py-28'>
-				<h1 className='font-bold text-4xl text-gray-100 dark:text-discord-dark'>한국 디스코드봇 리스트</h1>
+		<div className='bg-discord-blurple dark:bg-discord-black'>
+			<Container className='pt-20 pb-28'>
+				<h1 className='text-3xl text-gray-100 font-bold sm:text-left text-center'>한국 디스코드봇 리스트</h1>
 			</Container>
 		</div>
+		<Wave color='currentColor' className='text-discord-blurple dark:text-discord-black'/>
 	</>
 }
 

dcdaa97d0fad918d0f9819a7b03904fe9e96bd22
원더 <wonderbotsupport@naver.com>
2021-01-04 20:13:15 +0900

    feat: postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+	plugins: [
+		'tailwindcss',
+		[
+			'postcss-preset-env',
+			{
+				autoprefixer: {
+					flexbox: 'no-2009',
+				},
+				stage: 3,
+				features: {
+					'custom-properties': false,
+				},
+			},
+		],
+	],
 }

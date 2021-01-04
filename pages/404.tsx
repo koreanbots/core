340bed616f8afe870e48a98d19728d8aa15df403
원더 <wonderbotsupport@naver.com>
2021-01-04 20:11:44 +0900

    feat: added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import Container from '../components/Container'
+
+const NotFound: NextPage = () => {
+	return <Container>
+		<h1>GG</h1>
+	</Container>
+}
+
+export default NotFound

6886bd7a442600b8ceade9d7508347c6b209e489
원더 <wonderbotsupport@naver.com>
2021-01-04 22:09:00 +0900

    feat: added v2 api endpoint

---
+++ pages/api/v2/index.ts
@@ -0,0 +1,8 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+
+const HelloWorld:NextApiHandler = (_req: NextApiRequest, res: NextApiResponse) =>{
+	res.statusCode = 200
+	res.json({ happy: 'hacking' })
+}
+
+export default HelloWorld

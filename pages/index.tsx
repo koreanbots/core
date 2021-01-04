894dec5a737ccd3b4d4c51f4d6cdf8cb6c7090cd
원더 <wonderbotsupport@naver.com>
2021-01-04 20:13:00 +0900

    feat: added Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from 'next'
+import Container from '../components/Container'
 
 const Index: NextPage = () => {
-	return <div></div>
+	return <>
+		<div className='bg-discord-blurple dark:bg-discord-dark'>
+			<Container className='py-28'>
+				<h1 className='font-bold text-4xl text-gray-100 dark:text-discord-dark'>한국 디스코드봇 리스트</h1>
+			</Container>
+		</div>
+	</>
 }
 
 export default Index

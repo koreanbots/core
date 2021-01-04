dcc7a905150dfc751901bc2f8b189e5c1a3bcde7
원더 <wonderbotsupport@naver.com>
2021-01-04 22:11:13 +0900

    feat: added response wrapper util

---
+++ utils/ResponseWrapper.ts
@@ -0,0 +1,21 @@
+import http from 'http'
+import { NextApiResponse } from 'next'
+export default function ResponseWrapper(res: NextApiResponse, { code, message, version=2, data, errors }:ResponseProps) {
+	if(!code) throw new Error('`code` is required.')
+	if(!http.STATUS_CODES[code]) throw new Error('Invalid http code.')
+	res.statusCode = code
+  
+	return { code, message: message || http.STATUS_CODES[code], data, errors, version }
+}
+
+interface ResponseProps {
+  code: number
+  message?: string
+  version?: number
+  data?: Data
+  errors?: string[]
+}
+
+interface Data {
+  [key: string]: unknown
+}
---
+++ utils/index.ts
@@ -0,0 +1,3 @@
+import ResponseWrapper from './ResponseWrapper'
+
+export { ResponseWrapper }

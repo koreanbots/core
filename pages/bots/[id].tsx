648d99f8f5efce620750f3e878a8bdd22bf69f01
원더 <wonderbotsupport@naver.com>
2021-01-05 11:40:44 +0900

    fix: error

--- pages/bots/[id].tsx
+++ pages/bots/[id].tsx
@@ -1,10 +1,13 @@
 import { GetServerSideProps, NextPage } from 'next'
+import Container from '../../components/Container'
 
-const Bots = ():NextPage => {
-  
+const Bots:NextPage = () => {
+	return <Container>
+		<h1>GG</h1>
+	</Container>
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	return { a: 1 }
+	return { props: { data: {} } }
 }
 export default Bots

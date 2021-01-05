37ac2ea4dddce51a78ed4978a3e4d0e6b94569a6
원더 <wonderbotsupport@naver.com>
2021-01-05 11:34:46 +0900

    feat: added search component

---
+++ components/Search.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const Search = ({query, result}:SearchProps):JSX.Element => {
+	return (
+		<div>
+			<input />
+		</div>
+	)
+}
+
+interface SearchProps {
+  query: string
+  result: string
+}
+
+export default Search

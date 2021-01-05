c8570a7c7e780eb30cfc306eaff334044329346e
원더 <wonderbotsupport@naver.com>
2021-01-05 11:35:04 +0900

    dependencies: using dataloader

---
+++ pages/bots/[id].tsx
@@ -0,0 +1,10 @@
+import { GetServerSideProps, NextPage } from 'next'
+
+const Bots = ():NextPage => {
+  
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	return { a: 1 }
+}
+export default Bots

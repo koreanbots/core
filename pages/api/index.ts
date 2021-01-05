0d3efd3c7990c0982f68486f6bedb15a809eab43
원더 <wonderbotsupport@naver.com>
2021-01-05 10:30:54 +0900

    style: styled code

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,6 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 
-const HelloWorld:NextApiHandler = (_req: NextApiRequest, res: NextApiResponse) =>{
+const HelloWorld: NextApiHandler = (_req: NextApiRequest, res: NextApiResponse) => {
 	res.statusCode = 200
 	res.json({ happy: 'hacking' })
 }
--- utils/ResponseWrapper.ts
+++ utils/ResponseWrapper.ts
@@ -1,21 +1,24 @@
 import http from 'http'
 import { NextApiResponse } from 'next'
-export default function ResponseWrapper(res: NextApiResponse, { code, message, version=2, data, errors }:ResponseProps) {
-	if(!code) throw new Error('`code` is required.')
-	if(!http.STATUS_CODES[code]) throw new Error('Invalid http code.')
+export default function ResponseWrapper(
+	res: NextApiResponse,
+	{ code, message, version = 2, data, errors }: ResponseProps
+) {
+	if (!code) throw new Error('`code` is required.')
+	if (!http.STATUS_CODES[code]) throw new Error('Invalid http code.')
 	res.statusCode = code
-  
+
 	return { code, message: message || http.STATUS_CODES[code], data, errors, version }
 }
 
 interface ResponseProps {
-  code: number
-  message?: string
-  version?: number
-  data?: Data
-  errors?: string[]
+	code: number
+	message?: string
+	version?: number
+	data?: Data
+	errors?: string[]
 }
 
 interface Data {
-  [key: string]: unknown
+	[key: string]: unknown
 }

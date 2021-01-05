d1dc98b6621c7980df74f6def0f87bcafaee78b2
원더 <wonderbotsupport@naver.com>
2021-01-05 10:31:23 +0900

    style: styled code

--- pages/api/v1/index.ts
+++ pages/api/v1/index.ts
@@ -1,8 +1,12 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 import { ResponseWrapper } from '../../../utils'
 
-const Deprecated:NextApiHandler = (_req: NextApiRequest, res: NextApiResponse) =>{
-	const response = ResponseWrapper(res, { code: 406, message: '해당 API 버전은 지원 종료되었습니다.', version: 1 })
+const Deprecated: NextApiHandler = (_req: NextApiRequest, res: NextApiResponse) => {
+	const response = ResponseWrapper(res, {
+		code: 406,
+		message: '해당 API 버전은 지원 종료되었습니다.',
+		version: 1,
+	})
 	res.json(response)
 }
 

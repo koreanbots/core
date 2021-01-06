71f6d9691836348cce0cef08f1ba414cff1e7f58
원더 <wonderbotsupport@naver.com>
2021-01-07 01:15:16 +0900

    feat: added paddingTop props in Container

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,6 +1,6 @@
-const Container = ({ ignoreColor, className, children }:ContainerProps): JSX.Element => {
+const Container = ({ ignoreColor, className, paddingTop=false, children }:ContainerProps): JSX.Element => {
 	return (
-		<div className={ignoreColor ? null : 'bg-white dark:bg-discord-dark text-black dark:text-gray-100'}>
+		<div className={`${ignoreColor ? '' : 'bg-white dark:bg-discord-dark text-black dark:text-gray-100'} ${paddingTop ? 'pt-20' : ''}`}>
 			<div className={`container mx-auto px-4 ${className}`}>
 				{children}
 			</div>
@@ -11,6 +11,7 @@ const Container = ({ ignoreColor, className, children }:ContainerProps): JSX.Ele
 interface ContainerProps {
   ignoreColor?: boolean
   className?: string
+  paddingTop?: boolean
   children: JSX.Element | JSX.Element[]
 }
 

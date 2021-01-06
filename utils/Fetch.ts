dcb0d08bb4ba6ee3b1c058b3fe770b24dc51c960
원더 <wonderbotsupport@naver.com>
2021-01-07 01:21:24 +0900

    fix: fixed fetching sending RowpacketData class

--- utils/Fetch.ts
+++ utils/Fetch.ts
@@ -3,13 +3,13 @@ import * as Query from './Query'
 
 const Fetch = {
 	bot: new DataLoader(
-		async (ids: string[]) => await Promise.all(ids.map((el: string) => Query.getBot(el))),
+		async (ids: string[]) => (await Promise.all(ids.map((el: string) => Query.getBot(el)))).map(row => ({ ...row })),
 		{
 			batchScheduleFn: callback => setTimeout(callback, 1000),
 		}
 	),
 	user: new DataLoader(
-		async (ids: string[]) => await Promise.all(ids.map((el: string) => Query.getUser(el))),
+		async (ids: string[]) => (await Promise.all(ids.map((el: string) => Query.getUser(el)))).map(row => ({ ...row })),
 		{
 			batchScheduleFn: callback => setTimeout(callback, 1000),
 		}

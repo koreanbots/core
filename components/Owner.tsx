188e78e299478f884506662164371b0bafc9a29c
원더 <wonderbotsupport@naver.com>
2021-01-07 22:50:32 +0900

    feat: owner owner owner

--- components/Owner.tsx
+++ components/Owner.tsx
@@ -3,12 +3,21 @@ import DiscordImage from './DiscordImage'
 
 const Owner = ({ id, username, tag, avatarHash }:OwnerProps):JSX.Element => {
 	return <Link href={`/users/${id}`}>
-		<a className='text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 px-2 py-1 rounded mr-1 mb-1 hover:bg-little-white-hover dark:hover:bg-discord-dark-hover flex'>
-			<DiscordImage userID={id} tag={tag} avatarHash={avatarHash} className='rounded-full mr-4' size={30} />
-			<div className='ml-3 py-2'>
-				{username} <span className='text-gray-300 text-sm'>#{tag}</span>
+		<div className="text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover cursor-pointer px-4 py-4 mb-1">
+			<div className="rounded-full h-8 w-8 flex-shrink-0 mr-3 mt-1 overflow-hidden shadow-inner relative">
+				<DiscordImage userID={id} tag={tag} avatarHash={avatarHash} className="absolute inset-0 z-negative w-full h-full" size={50} />
+			</div> 
+			<div className="flex-1 leading-snug w-0">
+				<h4 className="whitespace-nowrap">{username}
+				</h4><span className="text-sm text-gray-600">#{tag}</span>
 			</div>
-		</a>
+		</div>
+		{/* <div className='text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 px-2 rounded mr-1 mb-3 hover:bg-little-white-hover dark:hover:bg-discord-dark-hover w-full py-5'>  
+			<DiscordImage userID={id} tag={tag} avatarHash={avatarHash} className='rounded-full float-left mr-4 py-' size={30} />
+			<span className='ml-3'>
+				{username} <span className='text-gray-300 text-sm mt-2'>#{tag}</span>
+			</span>
+		</div> */}
 	</Link>
 }
 

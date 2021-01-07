56e39a9e28e42c5053c5fd0e7a126dbb4c9e4bec
원더 <wonderbotsupport@naver.com>
2021-01-07 14:57:37 +0900

    feat: added Divider

---
+++ components/Divider.tsx
@@ -0,0 +1,5 @@
+const Divider = ({ className }:{ className?: string }) => {
+	return <div className={`my-2 px-5 ${className || ''}`} style={{ borderTop: '1px solid rgba(34,36,38,.15)', borderBottom: '1px solid hsla(0,0%,100%,.1)' }}/>
+}
+
+export default Divider

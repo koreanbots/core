59721c62ad03061e8cb0ad95b307c54e2c23d4ea
원더 <wonderbotsupport@naver.com>
2021-01-07 15:13:25 +0900

    feat: added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,13 @@ module.exports = {
 			colors: {
 				'koreanbots-blue': '#3366FF',
 				'koreanbots-black': '#2C2F33',
+				'little-white': '#fbfbfb',
+				'little-white-hover': '#f1f1f1',
 				'koreanbots-green': '#21BA45',
 				'koreanbots-red': '#DB2828',
 				'discord-blurple': '#7289DA',
 				'discord-dark': '#2C2F33',
+				'discord-dark-hover': '#383f48',
 				'discord-black': '#23272A',
 				'discord-pink': '#FF73FA'
 			}

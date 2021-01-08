51720c3df795c3215364ac7e111f6dda1caaf28e
원더 <wonderbotsupport@naver.com>
2021-01-08 22:19:21 +0900

    style: prettier

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,18 +1,25 @@
-const Container = ({ ignoreColor, className, paddingTop=false, children }:ContainerProps): JSX.Element => {
+const Container = ({
+	ignoreColor,
+	className,
+	paddingTop = false,
+	children,
+}: ContainerProps): JSX.Element => {
 	return (
-		<div className={`${ignoreColor ? '' : 'bg-white dark:bg-discord-dark text-black dark:text-gray-100'} ${paddingTop ? 'pt-20' : ''}`}>
-			<div className={`container mx-auto px-4 ${className}`}>
-				{children}
-			</div>
+		<div
+			className={`${
+				ignoreColor ? '' : 'bg-white dark:bg-discord-dark text-black dark:text-gray-100'
+			} ${paddingTop ? 'pt-20' : ''}`}
+		>
+			<div className={`container mx-auto px-4 ${className}`}>{children}</div>
 		</div>
 	)
 }
 
 interface ContainerProps {
-  ignoreColor?: boolean
-  className?: string
-  paddingTop?: boolean
-  children: JSX.Element | JSX.Element[]
+	ignoreColor?: boolean
+	className?: string
+	paddingTop?: boolean
+	children: JSX.Element | JSX.Element[]
 }
 
 export default Container

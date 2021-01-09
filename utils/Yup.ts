f12805187a039fbbc2ea215be5a4dd18a9e997a0
원더 <wonderbotsupport@naver.com>
2021-01-09 19:28:18 +0900

    feat: added botList argument schema

--- utils/Yup.ts
+++ utils/Yup.ts
@@ -1,9 +1,16 @@
 import * as Yup from 'yup'
+import YupKorean from 'yup-locales-ko'
+import { ListType } from '../types'
 
-export const botListArgument = Yup.object({
-	type: Yup.string().oneOf(['VOTE', 'TRUSTED', 'NEW', 'PARTNERED', 'CATEGOORY', 'SEARCH']).required(),
+Yup.setLocale(YupKorean)
+
+export const botListArgumentSchema = Yup.object({
+	type: Yup.string().oneOf(['VOTE', 'TRUSTED', 'NEW', 'PARTNERED', 'CATEGORY', 'SEARCH']).required(),
 	page: Yup.number().positive().integer().optional().default(1),
 	query: Yup.string().optional()
 })
 
+export interface botListArgument extends Yup.Asserts<typeof botListArgumentSchema>  {
+	type: ListType
+} 
 export default Yup

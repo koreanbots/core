a6940d3fe388fe1964ce1ea4218a90238d693b07
원더 <wonderbotsupport@naver.com>
2021-01-09 22:45:02 +0900

    feat: added file for pm2

---
+++ eco.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+	apps : [{
+		name: 'koreanbots',
+		script: 'npm start',
+		env: {
+			NODE_ENV: 'development',
+		},
+		env_production: {
+			NODE_ENV: 'production',
+		}
+	}]
+}

d33ac60155eec2bb47550e54b842dc14f5e76bd8
원더 <wonderbotsupport@naver.com>
2021-01-09 15:34:22 +0900

    style: fixed eslint

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -19,8 +19,8 @@ const Tag = ({
 							? 'bg-discord-blurple'
 							: 'bg-little-white-hover dark:bg-very-black'
 						: github
-						? 'bg-gray-900 text-white'
-						: 'bg-little-white dark:bg-discord-black'
+							? 'bg-gray-900 text-white'
+							: 'bg-little-white dark:bg-discord-black'
 				} ${!blurple && !github ? 'text-black dark:text-gray-400' : ''} ${
 					circular ? 'rounded-3xl px-2.5 py-1.5' : 'rounded px-2 py-1'
 				} mr-1 mb-${marginBottom} hover:bg-little-white-hover dark:hover:bg-discord-dark-hover`}
@@ -35,8 +35,8 @@ const Tag = ({
 					? blurple
 						? 'font-bg text-white bg-discord-blurple text-black'
 						: github
-						? 'bg-gray-900 text-white'
-						: 'bg-little-white-hover dark:bg-very-black'
+							? 'bg-gray-900 text-white'
+							: 'bg-little-white-hover dark:bg-very-black'
 					: 'bg-little-white dark:bg-discord-black'
 			} ${!blurple && !github ? 'text-black dark:text-gray-400' : ''} ${
 				circular ? 'rounded-3xl px-2.5 py-1.5' : 'rounded px-2 py-1'

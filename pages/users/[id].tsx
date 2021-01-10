ddf7f09df612473065e176fcb06651e3b141d5c6
원더 <wonderbotsupport@naver.com>
2021-01-10 12:53:59 +0900

    close: https://github.com/koreanbots/v2-testing/issues/14

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -77,7 +77,7 @@ const Users: NextPage<UserProps> = ({ data }) => {
 			</div>
 			<Divider />
 			<h2 className="mt-8 text-3xl font-bold">제작한 봇</h2>
-			<div className="grid gap-4 mt-20 lg:grid-cols-4">
+			<div className="grid gap-4 mt-20 2xl:grid-cols-4 md:grid-cols-2">
 				{(data.bots as Bot[]).map((bot: Bot) => (
 					<BotCard key={bot.id} bot={bot} />
 				))}

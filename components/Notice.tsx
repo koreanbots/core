5f11e2aa8ac3486e4a307774cd3f61f8fcd0e8d8
원더 <wonderbotsupport@naver.com>
2021-01-13 14:28:28 +0900

    style: Notice style change

--- components/Notice.tsx
+++ components/Notice.tsx
@@ -1,10 +1,10 @@
 const Notice = ({ header, desc }:NoticeProps) => {
 	return (
 		<div className='py-48 px-10 mx-auto my-auto h-screen text-center'>
-			<h1 className='text-3xl font-bold'>KOREANBOTS</h1>
+			<h1 className='text-4xl font-bold'>KOREANBOTS</h1>
 			<br />
 			<div>
-				<h1 className='text-2xl font-bold'>{header}</h1>
+				<h1 className='text-3xl font-bold mb-10'>{header}</h1>
 	
 				<h2 className='text-lg font-semibold'>{desc}</h2>
 				<br />

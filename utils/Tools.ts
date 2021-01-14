6d95807e64ab0e16d5fae9a6fe45346aede7660a
원더 <wonderbotsupport@naver.com>
2021-01-14 17:04:05 +0900

    feat: added checkBrowser method

--- utils/Tools.ts
+++ utils/Tools.ts
@@ -31,3 +31,20 @@ export function supportsWebP() {
 	// very old browser like IE 8, canvas not supported
 	return false
 }
+
+export function checkBrowser(){
+	const ua = navigator.userAgent
+	let tem
+	let M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+(\.\d+)?(\.\d+)?)/i) || []
+	if(/trident/i.test(M[1])){
+		tem=/\brv[ :]+(\d+)/g.exec(ua) || []
+		return 'IE '+(tem[1] || '')
+	}
+	if(M[1]=== 'Chrome'){
+		tem= ua.match(/\b(OPR|Edge|Whale)\/(\d+)/)
+		if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera')
+	}
+	M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?']
+	if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1])
+	return M.join(' ')
+}

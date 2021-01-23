830b9d067f9c47579022174114a08066682b04d3
원더 <wonderbotsupport@naver.com>
2021-01-23 11:46:41 +0900

    feat: search ui

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,9 @@
 // import { useState } from 'react'
 
-const Search = ({ query, result }: SearchProps): JSX.Element => {
+const Search = (): JSX.Element => {
 	return (
-		<div>
-			{query} { result }
-			<input />
+		<div className='relative w-full mt-5 flex'>
+			<input type='search' className='outline-none shadow rounded-lg border-0 p-3 w-full h-16 text-xl dark:bg-very-black pr-20' placeholder='검색...' />
 		</div>
 	)
 }

c8443ad441577323e7047f72f6fae4f1a617b59b
원더 <wonderbotsupport@naver.com>
2021-01-23 21:54:39 +0900

    fix: types

--- pages/api/v1/[[...deprecated]].ts
+++ pages/api/v1/[[...deprecated]].ts
@@ -1,7 +1,7 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 import ResponseWrapper from '@utils/ResponseWrapper'
 
-const Deprecated: NextApiHandler = (_req: NextApiRequest, res: NextApiResponse):void => {
+const Deprecated: NextApiHandler = (_req: NextApiRequest, res: NextApiResponse) => {
 	return ResponseWrapper(res, {
 		code: 406,
 		message: '해당 API 버전은 지원 종료되었습니다.',

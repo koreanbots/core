b2b4ef9de794bfd188221baad355b0dbb6a8f6f8
원더 <wonderbotsupport@naver.com>
2021-01-24 13:52:38 +0900

    chore: 404 page function name change

--- pages/api/[...404].ts
+++ pages/api/[...404].ts
@@ -1,8 +1,8 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 
-const HelloWorld: NextApiHandler = (_req: NextApiRequest, res: NextApiResponse) => {
+const NotFound: NextApiHandler = (_req: NextApiRequest, res: NextApiResponse) => {
 	res.statusCode = 404
 	res.json({ code: 404, message: '요청하신 URL에 페이지가 존재하지 않습니다.' })
 }
 
-export default HelloWorld
+export default NotFound

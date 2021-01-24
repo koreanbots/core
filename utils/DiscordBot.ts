603b50e9e4613826db6904c7b84cf4a421ce0ef9
원더 <wonderbotsupport@naver.com>
2021-01-24 13:53:27 +0900

    feat: add on ready

--- utils/DiscordBot.ts
+++ utils/DiscordBot.ts
@@ -2,6 +2,7 @@ import * as Discord from 'discord.js'
 
 const DiscordBot = new Discord.Client()
 
+DiscordBot.on('ready', () => console.log('I\'m Ready'))
 DiscordBot.login(process.env.DISCORD_TOKEN)
 
 export default DiscordBot

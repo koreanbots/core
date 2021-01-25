28d4fc116a46b3c69c517e42b60360400ddeb7a9
원더 <wonderbotsupport@naver.com>
2021-01-25 14:24:17 +0900

    fix: wrong prop

--- components/Owner.tsx
+++ components/Owner.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 import DiscordAvatar from './DiscordAvatar'
 
-const Owner = ({ id, username, tag, avatarHash }:OwnerProps):JSX.Element => {
+const Owner = ({ id, username, tag }:OwnerProps):JSX.Element => {
 	return <Link href={`/users/${id}`}>
 		<div className='text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover cursor-pointer px-4 py-4 mb-1'>
 			<div className='rounded-full h-8 w-8 flex-shrink-0 mr-3 mt-1 overflow-hidden shadow-inner relative'>
-				<DiscordAvatar userID={id} tag={tag} avatarHash={avatarHash} className='absolute inset-0 z-negative w-full h-full'/>
+				<DiscordAvatar userID={id} className='absolute inset-0 z-negative w-full h-full'/>
 			</div> 
 			<div className='flex-1 leading-snug w-0'>
 				<h4 className='whitespace-nowrap'>{username}

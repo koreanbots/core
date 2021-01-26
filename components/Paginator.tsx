0c3adbf9e54871bdae1eaf31afe60fdea8775b4b
원더 <wonderbotsupport@naver.com>
2021-01-26 11:37:55 +0900

    fix: paginator

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 const Paginator = ({ currentPage, totalPage }:PaginatorProps):JSX.Element => {
-	const pages = [1, currentPage === 1 ? 2 : currentPage - 1, currentPage === 1 ? 3 : totalPage === currentPage ? currentPage - 2 : currentPage, currentPage + 1, totalPage ]
+	const pages = [1, currentPage >= totalPage - 1 ? totalPage - 2 : currentPage - 1, currentPage <= 2 ? 2 : currentPage >= totalPage - 1 ? totalPage - 1 : currentPage, currentPage <= 2 ? 3 : currentPage + 1, totalPage  ]
 	return <div className='flex flex-col items-center my-12 text-center justify-center'>
 		<div className='flex text-gray-700 dark:text-gray-400'>
 			<Link href={{ pathname: '/list/votes', query: { page: currentPage - 1} }}>
@@ -11,14 +11,14 @@ const Paginator = ({ currentPage, totalPage }:PaginatorProps):JSX.Element => {
 			<div className='flex h-12 font-medium rounded-full bg-gray-200 dark:bg-discord-black'>
 				<a className={`w-12 md:flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in rounded-l-full ${pages[0] === currentPage ? 'bg-gray-300 dark:bg-discord-dark-hover' : ' hover:bg-gray-300 dark:hover:bg-discord-dark-hover'}`}>{pages[0]}</a>
 				{
-					pages[1] - pages[0] <= 1 ? '' : <>
+					pages[1] - pages[0] <= 1 ? pages[2] - pages[0] <= 1 ? '' : <a className='w-12 md:flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in'>...</a> : <>
 						<a className='w-12 md:flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in'>...</a>
 						<a className='w-12 md:flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in hover:bg-gray-300 dark:hover:bg-discord-dark-hover'>{pages[1]}</a>
 					</>
 				}
 				<a className={`w-12 md:flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in ${currentPage === 1 || currentPage === totalPage ? 'hover:bg-gray-300 dark:hover:bg-discord-dark-hover' : 'bg-gray-300 dark:bg-discord-dark-hover '}`}>{pages[2] === pages[4] ? pages[4] - 1 : pages[2]}</a>
 				{
-					pages[4] - pages[3] <= 1 ? '' : <>
+					pages[4] - pages[3] <= 1 ? pages[4] - pages[2] <= 1 ? '' : <a className='w-12 md:flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in'>...</a> : <>
 						<a className='w-12 md:flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in hover:bg-gray-300 dark:hover:bg-discord-dark-hover'>{pages[3]}</a>
 						<a className='w-12 md:flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in'>...</a>
 					</>

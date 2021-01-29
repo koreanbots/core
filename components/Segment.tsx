2f008e60e7e08a7bfe4deb641fe5325d3ebb5a25
원더 <wonderbotsupport@naver.com>
2021-01-29 20:24:48 +0900

    types: added segment props

--- components/Segment.tsx
+++ components/Segment.tsx
@@ -1,9 +1,13 @@
-const Segment = ({ children }): JSX.Element => {
+const Segment = ({ children }:SegmentProps): JSX.Element => {
 	return (
 		<div className='p-5 text-black dark:text-white dark:bg-discord-black bg-little-white rounded-sm'>
 			{children}
 		</div>
 	)
+}
+
+interface SegmentProps {
+	children: JSX.Element | JSX.Element[]
 }
 
 export default Segment

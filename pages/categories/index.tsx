ba5907d10b49ed458aed41b85966a26d00ca5f38
원더 <wonderbotsupport@naver.com>
2021-01-29 21:55:25 +0900

    feat: added categories page

---
+++ pages/categories/index.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from 'next'
+import dynamic from 'next/dynamic'
+
+import { categories, categoryIcon } from '@utils/Constants'
+
+const Container = dynamic(() => import('@components/Container'))
+const Tag = dynamic(() => import('@components/Tag'))
+const Segment = dynamic(() => import('@components/Segment'))
+
+const Categories:NextPage = () => {
+	return <Container paddingTop>
+		<h1 className='text-2xl font-bold mb-5'>전체 카테고리</h1>
+		<Segment>
+			<div className='text-center flex flex-wrap'>
+				{
+					categories.map(t => <Tag key={t} text={<>
+						<i className={categoryIcon[t]} /> {t}
+					</>} href={`/categories/${t}`} dark bigger /> )
+				}
+			</div>
+		</Segment>
+	</Container>
+}
+
+export default Categories

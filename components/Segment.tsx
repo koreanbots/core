fe41cd5e6e04ec2a3da4e74e4ba1e08f543291b8
원더 <wonderbotsupport@naver.com>
2021-01-30 12:19:51 +0900

    style: more padding x in segment

--- components/Segment.tsx
+++ components/Segment.tsx
@@ -1,6 +1,6 @@
 const Segment = ({ children }:SegmentProps): JSX.Element => {
 	return (
-		<div className='p-5 text-black dark:text-white dark:bg-discord-black bg-little-white rounded-sm'>
+		<div className='px-7 text-black dark:text-white dark:bg-discord-black bg-little-white rounded-sm'>
 			{children}
 		</div>
 	)

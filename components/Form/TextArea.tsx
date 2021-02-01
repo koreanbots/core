b283853e926f990399eb6ed0d6f8f98050ea2a44
원더 <wonderbotsupport@naver.com>
2021-02-01 20:48:48 +0900

    chore: className change for Textarea

--- components/Form/TextArea.tsx
+++ components/Form/TextArea.tsx
@@ -1,7 +1,7 @@
 import { Field } from 'formik'
 
 const TextArea = ({ name, placeholder }:TextAreaProps):JSX.Element => {
-	return <Field as='textarea' name={name} className='text-black w-full border border-grey-light rounded px-4 py-3 relative focus:border-blue focus:shadow min-h-2 max-h-3' placeholder={placeholder} />
+	return <Field as='textarea' name={name} className='text-black dark:bg-very-black dark:text-white w-full rounded px-4 py-3 relative min-h-2 max-h-3 outline-none' placeholder={placeholder} />
 }
 
 interface TextAreaProps {

3f0105e3f4544661ef8e63ef86afef6b0d7e0e3e
원더 <wonderbotsupport@naver.com>
2021-02-02 21:49:34 +0900

    fix: prefix regex

--- utils/Regex.ts
+++ utils/Regex.ts
@@ -2,6 +2,6 @@ import urlRegex from 'url-regex'
 
 export const ID = /[0-9]{17,}/
 export const Vanity = /[A-z|\d|-]+/
-export const Prefix = /^\s/
+export const Prefix = /^[^\s]/
 export const HTTPProtocol = /^https?:\/\/.*?/
 export const Url = urlRegex({ strict: true })

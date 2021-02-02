af0e1990cdaf5372b497ff7cd346f12897c866f9
원더 <wonderbotsupport@naver.com>
2021-02-02 21:00:37 +0900

    chore: fixed some regex and message
    close: https://github.com/koreanbots/v2-testing/issues/40

---
+++ utils/Regex.ts
@@ -0,0 +1,7 @@
+import urlRegex from 'url-regex'
+
+export const ID = /[0-9]{17,}/
+export const Vanity = /[A-z|\d|-]+/
+export const Prefix = /^\s/
+export const HTTPProtocol = /^https?:\/\/.*?/
+export const Url = urlRegex({ strict: true })
--- utils/Yup.ts
+++ utils/Yup.ts
@@ -2,6 +2,7 @@ import * as Yup from 'yup'
 import YupKorean from 'yup-locales-ko'
 import { ListType } from '../types'
 import { categories, library } from './Constants'
+import { HTTPProtocol, ID, Prefix, Url, Vanity } from './Regex'
 
 Yup.setLocale(YupKorean)
 
@@ -44,14 +45,14 @@ interface OauthCallback {
 
 export const AddBotSubmitSchema = Yup.object({
 	agree: Yup.boolean().oneOf([true], '상단의 체크박스를 클릭해주세요.').required('상단의 체크박스를 클릭해주세요.'),
-	id: Yup.string().matches(/[0-9]{17,}/, '올바른 봇 ID를 입력해주세요.').required('봇 ID는 필수 항목입니다.'),
-	prefix: Yup.string().min(1, '접두사는 최소 1자여야합니다.').max(32, '접두사는 최대 32자여야합니다.').required('접두사는 필수 항목입니다.'),
+	id: Yup.string().matches(ID, '올바른 봇 ID를 입력해주세요.').required('봇 ID는 필수 항목입니다.'),
+	prefix: Yup.string().matches(Prefix, '접두사는 띄어쓰기로 시작할 수 없습니다.').min(1, '접두사는 최소 1자여야합니다.').max(32, '접두사는 최대 32자까지만 가능합니다.').required('접두사는 필수 항목입니다.'),
 	library: Yup.string().oneOf(library).required('라이브러리는 필수 항목입니다.'),
-	website: Yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, '올바른 웹사이트 URL을 입력해주세요.'),
-	url: Yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, '올바른 초대링크 URL을 입력해주세요.'),
-	git: Yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, '올바른 깃을 입력해주세요.'),
-	discord: Yup.string().max(32, '지원 디스코드는 최대 32자여야합니다.'),
-	category: Yup.array(Yup.string().oneOf(categories)).min(1, '최소 한 개의 카테고리를 선택해주세요.').required(),
+	website: Yup.string().matches(HTTPProtocol, 'http:// 또는 https:// 로 시작해야합니다.').matches(Url, '올바른 웹사이트 URL을 입력해주세요.'),
+	url: Yup.string().matches(HTTPProtocol, 'http:// 또는 https:// 로 시작해야합니다.').matches(Url, '올바른 초대링크 URL을 입력해주세요.'),
+	git: Yup.string().matches(HTTPProtocol, 'http:// 또는 https:// 로 시작해야합니다.').matches(Url, '올바른 깃 URL을 입력해주세요.'),
+	discord: Yup.string().matches(Vanity, '디스코드 초대코드 형식을 지켜주세요.').min(2, '지원 디스코드는 최소 2자여야합니다.').max(32, '지원 디스코드는 최대 32자까지만 가능합니다.'),
+	category: Yup.array(Yup.string().oneOf(categories)).min(1, '최소 한 개의 카테고리를 선택해주세요.').required('카테고리는 필수 항목입니다.'),
 	intro: Yup.string().min(2, '봇 소개는 최소 2자여야합니다.').max(60, '봇 소개는 최대 60자여야합니다.').required('봇 소개는 필수 항목입니다.'),
 	desc: Yup.string().min(100, '봇 설명은 최소 100자여야합니다.').max(1500, '봇 설명은 최대 1500자여야합니다.').required('봇 설명은 필수 항목입니다.')
 })

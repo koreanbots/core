555d1d17f8ebc65e1fb13ddefa5a78c4d03a58dd
원더 <wonderbotsupport@naver.com>
2021-02-03 22:16:23 +0900

    chore: made some changes for Docs

--- components/Docs.tsx
+++ components/Docs.tsx
@@ -5,21 +5,21 @@ const Docs = ({ header, description, subheader, children }: DocsProps): JSX.Elem
 	return (
 		<>
 			<div className='dark:bg-discord-black bg-discord-blurple'>
-				<Container className='pb-28 pt-20' ignoreColor>
+				<Container className='pb-10 pt-20' ignoreColor>
 					<h1 className='mt-10 text-center text-gray-100 text-4xl font-bold sm:text-left'>
 						{header}
 					</h1>
-					<h2 className='mt-5 text-center text-gray-200 text-2xl font-medium sm:text-left'>
+					<h2 className='mt-5 text-center text-gray-200 text-xl font-medium sm:text-left'>
 						{description}
 					</h2>
-					<h2 className='mt-5 text-center text-gray-200 text-2xl font-medium sm:text-left'>
+					<h2 className='mt-5 text-center text-gray-200 text-xl font-medium sm:text-left'>
 						{subheader}
 					</h2>
 				</Container>
 			</div>
 			<Wave
 				color='currentColor'
-				className='dark:text-discord-black text-discord-blurple dark:bg-discord-dark bg-white'
+				className='dark:text-discord-black text-discord-blurple dark:bg-discord-dark bg-white hidden md:block'
 			/>
 			<Container>
 				<div>{children}</div>
@@ -31,7 +31,7 @@ const Docs = ({ header, description, subheader, children }: DocsProps): JSX.Elem
 export default Docs
 
 interface DocsProps {
-	header: string
+	header: string | JSX.Element
 	description?: string
 	subheader?: string
 	children: JSX.Element | JSX.Element[]

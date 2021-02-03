b9af7c43677ed17a24aa74e859ec64eef3dda364
원더 <wonderbotsupport@naver.com>
2021-02-03 22:14:42 +0900

    feat: add colorCard component

---
+++ components/ColorCard.tsx
@@ -0,0 +1,18 @@
+const ColorCard = ({ header, first, second, className }:ColorCardProps):JSX.Element => {
+	return <div className={`rounded-lg p-10 ${className} shadow-lg`}>
+		<h2 className='text-2xl font-bold'>{header}</h2>
+		<p className='opacity-80'>
+			{first} <br/>
+			{second}
+		</p>
+	</div>
+}
+
+interface ColorCardProps {
+  header: string
+  first: string
+  second: string
+  className: string
+}
+
+export default ColorCard

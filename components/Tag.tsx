bd6cc598e0453482f10063cfcffdecbbb1f93b7e
원더 <wonderbotsupport@naver.com>
2021-02-04 11:04:51 +0900

    fix: github button
    close: https://github.com/koreanbots/v2-testing/issues/9

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -24,7 +24,7 @@ const Tag = ({
 						? 'bg-discord-blurple text-white'
 						: 'bg-little-white-hover hover:bg-little-white dark:bg-very-black'
 					: github
-						? 'bg-gray-900 text-white'
+						? 'bg-gray-900 text-white hover:bg-gray-700'
 						: 'bg-little-white dark:bg-discord-black hover:bg-little-white-hover'
 			} ${!blurple && !github ? 'text-black dark:text-gray-400' : ''} ${
 				circular ? `rounded-3xl ${bigger ? 'px-3.5 py-2.5' : 'px-2.5 py-1.5'}` : `rounded ${bigger ? 'px-3 py-2' : 'px-2 py-1'}`
@@ -42,7 +42,7 @@ const Tag = ({
 							? 'bg-discord-blurple text-white'
 							: 'bg-little-white-hover hover:bg-little-white dark:bg-very-black'
 						: github
-							? 'bg-gray-900 text-white'
+							? 'bg-gray-900 text-white hover:bg-gray-700'
 							: 'bg-little-white dark:bg-discord-black hover:bg-little-white-hover'
 				} ${!blurple && !github ? 'text-black dark:text-gray-400' : 'hover:bg-little-white-hover'} ${
 					circular ? `rounded-3xl ${bigger ? 'px-3.5 py-2.5' : 'px-2.5 py-1.5'}` : `rounded ${bigger ? 'px-3 py-2' : 'px-2 py-1'}`
@@ -59,7 +59,7 @@ const Tag = ({
 					? blurple
 						? 'font-bg bg-discord-blurple text-white'
 						: github
-							? 'bg-gray-900 text-white'
+							? 'bg-gray-900 text-white hover:bg-gray-700'
 							: `bg-little-white-hover dark:bg-very-black ${props.onClick ? 'hover:bg-little-white dark:hover:bg-discord-dark-hover transition duration-100 ease-in' : '' }`
 					: `bg-little-white dark:bg-discord-black ${props.onClick ? 'hover:bg-little-white-hover dark:hover:bg-discord-dark-hover transition duration-100 ease-in' : '' }`
 			} ${!blurple && !github ? 'text-black dark:text-gray-400' : ''} ${

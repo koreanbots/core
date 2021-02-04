3a4d78be8a3dcf416e62cd41c54f6025883100ba
원더 <wonderbotsupport@naver.com>
2021-02-04 12:30:12 +0900

    fix: ads

--- components/Advertisement.tsx
+++ components/Advertisement.tsx
@@ -1,7 +1,7 @@
 const Advertisement = (): JSX.Element => {
-	if (process.env.NODE_ENV === 'production')
-		return (
-			<ins
+	return <div className='my-5 mx-auto py-12 w-full text-center text-white bg-gray-700'>
+		{
+			process.env.NODE_ENV === 'production' ?  <ins
 				className='adsbygoogle mb-5 w-full'
 				style={{ display: 'block' }}
 				data-ad-client='ca-pub-4856582423981759'
@@ -9,10 +9,8 @@ const Advertisement = (): JSX.Element => {
 				data-ad-format='auto'
 				data-adtest='on'
 				data-full-width-responsive='true'
-			></ins>
-		)
-	else
-		return <div className='my-5 py-12 w-full text-center text-white bg-gray-700'>Advertisement</div>
+			></ins> : 'Advertisement'
+		}</div>
 }
 
 export default Advertisement

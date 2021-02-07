a4601c7f49a25fa9a5fcfeac7a0b2baca5fd8e68
원더 <wonderbotsupport@naver.com>
2021-02-07 22:54:36 +0900

    fix: header anchor at emoji

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -5,7 +5,7 @@ import Emoji from 'node-emoji'
 
 const Markdown = ({ text }:MarkdownProps):JSX.Element => {
 	return <div className='w-full markdown-body'>
-		<MarkdownView markdown={Emoji.emojify(text)} extensions={[ anchorHeader, twemoji, customEmoji ]} options={{ openLinksInNewWindow: true, underline: true, omitExtraWLInCodeBlocks: true, literalMidWordUnderscores: true, simplifiedAutoLink: true, tables: true, strikethrough: true, smoothLivePreview: true, tasklists: true, ghCompatibleHeaderId: true, encodeEmails: true }} sanitizeHtml={(html)=> sanitizeHtml(html, {
+		<MarkdownView markdown={Emoji.emojify(text)} extensions={[ twemoji, customEmoji, anchorHeader ]} options={{ openLinksInNewWindow: true, underline: true, omitExtraWLInCodeBlocks: true, literalMidWordUnderscores: true, simplifiedAutoLink: true, tables: true, strikethrough: true, smoothLivePreview: true, tasklists: true, ghCompatibleHeaderId: true, encodeEmails: true }} sanitizeHtml={(html)=> sanitizeHtml(html, {
 			allowedTags: [
 				'addr', 'address', 'article', 'aside', 'h1', 'h2', 'h3', 'h4',
 				'h5', 'h6', 'section', 'blockquote', 'dd', 'div',
--- utils/ShowdownExtensions.ts
+++ utils/ShowdownExtensions.ts
@@ -1,4 +1,4 @@
-import { Emoji, EmojiSyntax, Heading } from './Regex'
+import { Emoji, EmojiSyntax, Heading, ImageTag } from './Regex'
 import Twemoji from 'twemoji'
 import { KoreanbotsEmoji } from './Constants'
 
@@ -8,13 +8,14 @@ export const anchorHeader = {
 	replace: function (__match: string, id:string, title:string, level:number) {
 
 		// github anchor style
-		const octicon_html = `<a class="anchor mr-1 align-middle" aria-hidden="true" href="#${id}">
+		const href = id.replace(ImageTag, '$1').replace(/"/gi, '')
+		const octicon_html = `<a class="anchor mr-1 align-middle" aria-hidden="true" href="#${href}">
 			<svg fill="currentColor" class="octicon-link align-middle" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
 			<path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
 			</svg>
 		</a>`
 
-		return `<h${level} id="${id}">${octicon_html}${title}</h${level}>`
+		return `<h${level} id="${href}">${octicon_html}${title}</h${level}>`
 	}
 }
 
@@ -23,8 +24,10 @@ export const twemoji = {
 	regex: `${Emoji}{2}|${Emoji}`,
 	replace: function(__match: string, two: string, one: string) {
 		const parsed = __match || two || one
+		console.log(parsed)
 		const emoj = Twemoji.parse(parsed, { folder: 'svg', ext: '.svg' })
 		if(!emoj) return parsed
+		console.log(emoj)
 		return emoj
 	} 
 }

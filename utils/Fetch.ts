e5b576fec71df446ea8e58762c5f70525f2c05ae
원더 <wonderbotsupport@naver.com>
2021-02-08 22:40:41 +0900

    feat: added Fetch function

---
+++ utils/Fetch.ts
@@ -0,0 +1,20 @@
+import { ResponseProps } from '@types'
+import { KoreanbotsEndPoints } from './Constants'
+
+const Fetch = async <T>(endpoint: string, options?: RequestInit):Promise<ResponseProps<T>> => {
+	const url = KoreanbotsEndPoints.baseAPI + ( endpoint.startsWith('/') ? endpoint : '/' + endpoint)
+  
+	const res = await fetch(url, options ?? { method: 'GET' })
+  
+	let json = {}
+
+	try {
+		json = await res.json()
+	} catch {
+		json = { code: 500, message: (await res.text()) }
+	}
+  
+	return json
+}
+
+export default Fetch

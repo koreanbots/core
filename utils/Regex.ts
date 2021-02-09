781be673bb50ca5853e966032780f8b32ab46424
원더 <wonderbotsupport@naver.com>
2021-02-09 21:51:00 +0900

    fix: fixed prefix regex
    close: https://github.com/koreanbots/v2-testing/issues/48

--- utils/Regex.ts
+++ utils/Regex.ts
@@ -2,7 +2,7 @@ import urlRegex from 'url-regex-safe'
 
 export const ID = /^[0-9]{17,}$/
 export const Vanity = /^[A-Za-z\d-]+$/
-export const Prefix = /^[^\s]$/
+export const Prefix = /^[^\s]/
 export const HTTPProtocol = /^https?:\/\/.*?/
 export const Url = urlRegex({ strict: true })
 

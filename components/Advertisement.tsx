a910c688609cefc9eaa61164534d691118edbc33
원더 <wonderbotsupport@naver.com>
2021-02-11 19:28:46 +0900

    fix: advertisement error at development

--- components/Advertisement.tsx
+++ components/Advertisement.tsx
@@ -1,7 +1,10 @@
 import { useEffect } from 'react'
 
 const Advertisement = (): JSX.Element => {
-	useEffect(() => window.adsbygoogle.push({}), [])
+	useEffect(() => {
+		if(process.env.NODE_ENV === 'production')  window.adsbygoogle.push({}), []
+	})
+	
 	return <div className={`z-0 my-1 mx-auto py-12 w-full text-center text-white ${process.env.NODE_ENV === 'production' ? '' : 'bg-gray-700'}`}>
 		{
 			process.env.NODE_ENV === 'production' ?  <ins

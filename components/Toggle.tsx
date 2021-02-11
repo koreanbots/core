0767d8d5bc368bc96633f2f0dee40534bdedc03d
원더 <wonderbotsupport@naver.com>
2021-02-11 21:04:32 +0900

    feat: added readOnly at Toggle

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,6 +1,6 @@
 const Toggle = ({ checked, onChange }:ToggleProps):JSX.Element => {
 	return <button className='relative inline-block w-10 mr-2 align-middle select-none outline-none' onClick={onChange} onKeyPress={onChange}>
-		<input type='checkbox' checked={checked} className='absolute block w-6 h-6 rounded-full bg-white border-4 border-transparent appearance-none cursor-pointer outline-none checked:right-0'/>
+		<input type='checkbox' checked={checked} className='absolute block w-6 h-6 rounded-full bg-white border-4 border-transparent appearance-none cursor-pointer outline-none checked:right-0' readOnly />
 		<span className={`block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer ${checked ? 'bg-koreanbots-blue' : ''}`}></span>
 	</button>
 }

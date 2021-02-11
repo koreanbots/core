b45be73aef4ab0a2c498d3ad56f2454823fb4a6c
원더 <wonderbotsupport@naver.com>
2021-02-11 22:47:50 +0900

    test: ad

--- components/Advertisement.tsx
+++ components/Advertisement.tsx
@@ -5,14 +5,13 @@ const Advertisement = (): JSX.Element => {
 		if(process.env.NODE_ENV === 'production')  window.adsbygoogle.push({})
 	})
 	
-	return <div className={`z-0 mx-auto w-full text-center text-white ${process.env.NODE_ENV === 'production' ? '' : 'py-12 bg-gray-700'}`} style={{ height: '120px' }}>
+	return <div className={`z-0 mx-auto w-full text-center text-white ${process.env.NODE_ENV === 'production' ? '' : 'py-12 bg-gray-700'}`} style={{ height: '90px' }}>
 		{
 			process.env.NODE_ENV === 'production' ?  <ins
 				className='adsbygoogle mb-5 w-full'
-				style={{ display: 'block' }}
+				style={{ display: 'inline-block', height: '90px' }}
 				data-ad-client='ca-pub-4856582423981759'
 				data-ad-slot='3250141451'
-				data-ad-format='auto'
 				data-adtest='on'
 				data-full-width-responsive='true'
 			></ins> : 'Advertisement'

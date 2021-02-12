5bd2c7622e6038e61ceac9f4281afb98afa844a9
원더 <wonderbotsupport@naver.com>
2021-02-12 11:36:22 +0900

    chore: added className prop at Segment

--- components/Segment.tsx
+++ components/Segment.tsx
@@ -1,12 +1,13 @@
-const Segment = ({ children }:SegmentProps): JSX.Element => {
+const Segment = ({ children, className='' }:SegmentProps): JSX.Element => {
 	return (
-		<div className='py-3 px-7 text-black dark:text-white dark:bg-discord-black bg-little-white rounded-sm'>
+		<div className={`py-3 px-7 text-black dark:text-white dark:bg-discord-black bg-little-white rounded-sm ${className}`}>
 			{children}
 		</div>
 	)
 }
 
 interface SegmentProps {
+	className?: string
 	children: JSX.Element | JSX.Element[]
 }
 

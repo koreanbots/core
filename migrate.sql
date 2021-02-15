47414d875e5d84acd8e38d07d0a12328e3442c52
Junseo Park <wonderlandpark@outlook.kr>
2021-02-15 11:32:05 +0900

    chore: updated sql

--- migrate.sql
+++ migrate.sql
@@ -5,14 +5,14 @@ use discordbots;
 ALTER TABLE `bots` CHANGE `servers` `servers` INT(11) NULL DEFAULT NULL, CHANGE `web` `web` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL, CHANGE `git` `git` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL, CHANGE `url` `url` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL, CHANGE `category` `category` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '\'[]\'', CHANGE `status` `status` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `avatar` `avatar` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL, CHANGE `tag` `tag` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, CHANGE `discord` `discord` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL, CHANGE `state` `state` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '\'ok\'', CHANGE `vanity` `vanity` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `bg` `bg` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `banner` `banner` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
 
 -- USING NULL
-UPDATE `bots` SET web=NULL where web='false';
-UPDATE `bots` SET git=NULL where git='false';
-UPDATE `bots` SET `url`=NULL where url='false';
-UPDATE `bots` SET avatar=NULL where avatar='false';
-UPDATE `bots` SET discord=NULL where discord='false';
-UPDATE `bots` SET vanity=NULL where vanity='false';
-UPDATE `bots` SET bg=NULL where bg='false';
-UPDATE `bots` SET banner=NULL where banner='false';
+UPDATE `bots` SET web=NULL where web='false' or web='';
+UPDATE `bots` SET git=NULL where git='false' or git='';
+UPDATE `bots` SET `url`=NULL where `url`='false' or `url`='';
+UPDATE `bots` SET avatar=NULL where avatar='false' or avatar='';
+UPDATE `bots` SET discord=NULL where discord='false' or discord='';
+UPDATE `bots` SET vanity=NULL where vanity='false' or vanity='';
+UPDATE `bots` SET bg=NULL where bg='false' or bg='';
+UPDATE `bots` SET banner=NULL where banner='false' or banner='';
 ALTER TABLE `bots` ADD COLUMN partnered BOOLEAN NOT NULL DEFAULT 0;
 ALTER TABLE `bots` CHANGE id id VARCHAR(50) NOT NULL PRIMARY KEY;
 ALTER TABLE `bots` ENGINE=mroonga;
@@ -41,10 +41,10 @@ ALTER TABLE `submitted` DROP `verified`;
 ALTER TABLE `submitted` DROP `trusted`;
 ALTER TABLE `submitted` DROP `avatar`;
 ALTER TABLE `submitted` ADD `reason` TINYTEXT NULL DEFAULT NULL;  
-UPDATE `submitted` SET web=NULL where web='false';
-UPDATE `submitted` SET git=NULL where git='false';
-UPDATE `submitted` SET `url`=NULL where url='false';
-UPDATE `submitted` SET discord=NULL where discord='false';
+UPDATE `submitted` SET web=NULL where web='false' or web='';
+UPDATE `submitted` SET git=NULL where git='false' or git='';
+UPDATE `submitted` SET `url`=NULL where `url`='false' or `url`='';
+UPDATE `submitted` SET discord=NULL where discord='false' or discord='';
 
 -- reports TABLE
 

ce8faf5d62fe8cc6aa7da3215ee988f52bb6daf9
Junseo Park <wonderlandpark@outlook.kr>
2021-02-15 11:42:39 +0900

    chore: added margin style

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Index: NextPage<IndexProps> = ({ votes, newBots, trusted }) => {
 			<Hero />
 			<Container>
 				<Advertisement />
-				<h1 className='text-3xl font-bold'>
+				<h1 className='text-3xl font-bold mt-10 mb-2'>
 					<i className='far fa-heart mr-3 text-pink-600' /> 하트 랭킹
 				</h1>
 				<p className='text-base'>하트를 많이 받은 봇들의 순위입니다!</p>
@@ -27,7 +27,8 @@ const Index: NextPage<IndexProps> = ({ votes, newBots, trusted }) => {
 					}
 				</div>
 				<Paginator totalPage={votes.totalPage} currentPage={votes.currentPage} pathname='/list/votes' />
-				<h1 className='text-3xl font-bold mt-20'>
+				<Advertisement />
+				<h1 className='text-3xl font-bold mt-20 mb-2'>
 					<i className='far fa-star mr-3 text-yellow-500' /> 새로운 봇
 				</h1>
 				<p className='text-base'>최근에 한국 디스코드봇 리스트에 추가된 따끈따끈한 봇입니다.</p>
@@ -37,7 +38,7 @@ const Index: NextPage<IndexProps> = ({ votes, newBots, trusted }) => {
 					}
 				</div>
 				<LongButton href='/list/new' center>더보기</LongButton>
-				<h1 className='text-3xl font-bold'>
+				<h1 className='text-3xl font-bold mb-2'>
 					<i className='fa fa-check mr-3 mt-10 text-green-500' /> 신뢰된 봇
 				</h1>
 				<p className='text-base'>KOREANBOTS에서 인증받은 신뢰할 수 있는 봇들입니다!!</p>

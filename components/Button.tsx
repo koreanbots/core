1bf4e8fb52524c1183297e61e5136db1eedc7503
Junseo Park <wonderlandpark@outlook.kr>
2021-02-15 12:56:16 +0900

    chore: className update props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 
-const Button = ({ type='button', className='', children, href, onClick }: ButtonProps):JSX.Element => {
+const Button = ({ type='button', className, children, href, onClick }: ButtonProps):JSX.Element => {
 	return href ? <Link href={href}>
-		<a className={`cursor-pointer bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className}`}>
+		<a className={`cursor-pointer rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
 			{ children }
 		</a>
-	</Link> : onClick ? <button type={type} onKeyDown={onClick} onClick={onClick} className={`cursor-pointer bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className}`}>
+	</Link> : onClick ? <button type={type} onKeyDown={onClick} onClick={onClick} className={`cursor-pointer rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
 		{ children }
-	</button> : <button type={type} className={`cursor-pointer bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className}`}>
+	</button> : <button type={type} className={`cursor-pointer rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
 		{ children }
 	</button>
 }
@@ -15,7 +15,7 @@ const Button = ({ type='button', className='', children, href, onClick }: Button
 interface ButtonProps {
   type?: 'button' | 'submit' | 'reset'
   className?: string
-  children: JSX.Element | JSX.Element[] | string
+  children: JSX.Element | string
   href?: string
   onClick?: () => void
 }

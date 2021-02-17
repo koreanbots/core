17b965547e9b351886522fb4fc7677382805e951
Junseo Park <wonderlandpark@outlook.kr>
2021-02-17 15:17:14 +0900

    chore: not sending message at no data

--- utils/ResponseWrapper.ts
+++ utils/ResponseWrapper.ts
@@ -1,6 +1,7 @@
 import http from 'http'
 import { NextApiResponse } from 'next'
 import { ResponseProps } from '@types'
+import { ErrorText } from './Constants'
 export default function ResponseWrapper<T=unknown>(
 	res: NextApiResponse,
 	{ code=200, message, version = 2, data, errors }: ResponseProps<T>
@@ -9,6 +10,6 @@ export default function ResponseWrapper<T=unknown>(
 	if (!http.STATUS_CODES[code]) throw new Error('Invalid http code.')
 	res.statusCode = code
 	res.setHeader('Access-Control-Allow-Origin', process.env.KOREANBOTS_URL)
-	res.json({ code, message: message || http.STATUS_CODES[code], data, errors, version })
+	res.json({ code, data, errors, version, ...(message || !data ? { message: message || ErrorText[code] || http.STATUS_CODES[code] } : {}) })
 }
 

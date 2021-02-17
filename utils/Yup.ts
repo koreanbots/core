c6606f0e9b95e92bbef68cf521734afd8ce6cd8c
Junseo Park <wonderlandpark@outlook.kr>
2021-02-17 15:16:19 +0900

    types: added AddBotSubmit schema type

--- utils/Yup.ts
+++ utils/Yup.ts
@@ -63,7 +63,7 @@ interface SearchQuery {
 	page: number
 }
 
-export const AddBotSubmitSchema = Yup.object({
+export const AddBotSubmitSchema: Yup.SchemaOf<AddBotSubmit> = Yup.object({
 	agree: Yup.boolean().oneOf([true], '상단의 체크박스를 클릭해주세요.').required('상단의 체크박스를 클릭해주세요.'),
 	id: Yup.string().matches(ID, '올바른 봇 ID를 입력해주세요.').required('봇 ID는 필수 항목입니다.'),
 	prefix: Yup.string().matches(Prefix, '접두사는 띄어쓰기로 시작할 수 없습니다.').min(1, '접두사는 최소 1자여야합니다.').max(32, '접두사는 최대 32자까지만 가능합니다.').required('접두사는 필수 항목입니다.'),
@@ -74,8 +74,23 @@ export const AddBotSubmitSchema = Yup.object({
 	discord: Yup.string().matches(Vanity, '디스코드 초대코드 형식을 지켜주세요.').min(2, '지원 디스코드는 최소 2자여야합니다.').max(32, '지원 디스코드는 최대 32자까지만 가능합니다.'),
 	category: Yup.array(Yup.string().oneOf(categories)).min(1, '최소 한 개의 카테고리를 선택해주세요.').required('카테고리는 필수 항목입니다.'),
 	intro: Yup.string().min(2, '봇 소개는 최소 2자여야합니다.').max(60, '봇 소개는 최대 60자여야합니다.').required('봇 소개는 필수 항목입니다.'),
-	desc: Yup.string().min(100, '봇 설명은 최소 100자여야합니다.').max(1500, '봇 설명은 최대 1500자여야합니다.').required('봇 설명은 필수 항목입니다.')
+	desc: Yup.string().min(100, '봇 설명은 최소 100자여야합니다.').max(1500, '봇 설명은 최대 1500자여야합니다.').required('봇 설명은 필수 항목입니다.'),
+	_csrf: Yup.string().required()
 })
 
+export interface AddBotSubmit {
+	agree: boolean
+	id: string
+	prefix: string
+	library: string
+	website?: string
+	url?: string
+	git?: string
+	discord?: string
+	category: string | string[]
+	intro: string
+	desc: string
+	_csrf: string
+}
 
 export default Yup

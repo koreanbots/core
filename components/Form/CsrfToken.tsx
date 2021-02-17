5769f90fbb5c096c24c214b8411b3cd6e7be1108
Junseo Park <wonderlandpark@outlook.kr>
2021-02-17 15:11:25 +0900

    feat: added CsrfToken input

---
+++ components/Form/CsrfToken.tsx
@@ -0,0 +1,11 @@
+import { Field } from 'formik'
+
+const CsrfToken = ({ token }:CsrfTokenProps):JSX.Element => {
+	return <Field name='_csrf' hidden value={token} readOnly />
+}
+
+interface CsrfTokenProps {
+  token: string
+}
+
+export default CsrfToken

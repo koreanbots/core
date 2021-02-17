2bc8ab8e0dea648e1cf45f6f32acce11340d3275
Junseo Park <wonderlandpark@outlook.kr>
2021-02-17 15:57:14 +0900

    feat: added add bot endpoint
    ON DEVELOPMENT

--- pages/api/v2/bots/[id].ts
+++ pages/api/v2/bots/[id].ts
@@ -1,7 +1,10 @@
-import { get } from '@utils/Query'
-import ResponseWrapper from '@utils/ResponseWrapper'
 import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
+
+import { get } from '@utils/Query'
+import ResponseWrapper from '@utils/ResponseWrapper'
+import { checkToken } from '@utils/Csrf'
+import { AddBotSubmit, AddBotSubmitSchema } from '@utils/Yup'
 
 const BotInfo = nc<ApiRequest, NextApiResponse>()
 	.get(async(req, res) => {
@@ -9,8 +12,28 @@ const BotInfo = nc<ApiRequest, NextApiResponse>()
 		if(!bot) return ResponseWrapper(res, { code: 404, message: '존재하지 않는 봇입니다.' })
 		else return ResponseWrapper(res, { code: 200, data: bot })
 	})
+	.post(async (req, res) => {
+		const user = await get.Authorization(req.cookies.token)
+		console.log(user)
+		if(!user) return ResponseWrapper(res, { code: 401 })
+		const csrfValidated = checkToken(req, res, req.body._csrf)
+		if(!csrfValidated) return
+
+		const validated = await AddBotSubmitSchema.validate(req.body, { abortEarly: false }).then(el => el).catch(e => {
+			ResponseWrapper(res, { code: 400, errors: e.errors })
+			return null
+		})
+
+		if(!validated) return
+		if(validated.id !== req.query.id) return ResponseWrapper(res, { code: 400, errors: ['요청 주소와 Body의 정보가 다릅니다.'] })
+		return ResponseWrapper(res, { code: 200, data: { url: '/pendingBots/00000/00000' } })
+	})
+	.patch(async (req, res) => {
+		return res.send('Reserved')
+	})
 
 interface ApiRequest extends NextApiRequest {
+	body: AddBotSubmit
 	query: {
 		id: string
 	}

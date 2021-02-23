60ddce77beb3dec568e00fcf1e186a4d585ce43b
Junseo Park <wonderlandpark@outlook.kr>
2021-02-23 09:56:14 +0900

    chore: mobile responsive form style improved

--- components/Form/Label.tsx
+++ components/Form/Label.tsx
@@ -1,6 +1,6 @@
 const Label = ({ For, children, label, labelDesc, error=null, grid=true, short=false, required=false }:LabelProps):JSX.Element => {
 	return <>
-		<label className={grid ? 'grid grid-cols-1 md:grid-cols-4 gap-2 my-4' : 'inline-flex items-center'} htmlFor={For}>
+		<label className={grid ? 'grid grid-cols-1 xl:grid-cols-4 gap-2 my-4' : 'inline-flex items-center'} htmlFor={For}>
 			{
 				label && <div className='col-span-1 text-sm'>
 					<h3 className='text-lg font-bold text-discord-blurple'>{label}

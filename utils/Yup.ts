46ef410f6f39637d3fd3a3de4de3cf5310c9eacd
Junseo Park <wonderlandpark@outlook.kr>
2021-02-23 10:00:00 +0900

    feat: added ManageBotSchema

--- utils/Yup.ts
+++ utils/Yup.ts
@@ -5,6 +5,11 @@ import { categories, library } from './Constants'
 import { HTTPProtocol, ID, Prefix, Url, Vanity } from './Regex'
 
 Yup.setLocale(YupKorean)
+Yup.addMethod(Yup.array, 'unique', function(message, mapper = a => a) {
+	return this.test('unique', message || 'array must be unique', function(list) {
+		return list.length  === new Set(list.map(mapper)).size
+	})
+})
 
 export const botListArgumentSchema: Yup.SchemaOf<botListArgument> = Yup.object({
 	type: Yup.mixed().oneOf(['VOTE', 'TRUSTED', 'NEW', 'PARTNERED', 'CATEGORY', 'SEARCH']).required(),
@@ -93,4 +98,17 @@ export interface AddBotSubmit {
 	_csrf: string
 }
 
+export const ManageBotSchema = Yup.object({
+	prefix: Yup.string().matches(Prefix, '접두사는 띄어쓰기로 시작할 수 없습니다.').min(1, '접두사는 최소 1자여야합니다.').max(32, '접두사는 최대 32자까지만 가능합니다.').required('접두사는 필수 항목입니다.'),
+	library: Yup.string().oneOf(library).required('라이브러리는 필수 항목입니다.'),
+	website: Yup.string().matches(HTTPProtocol, 'http:// 또는 https:// 로 시작해야합니다.').matches(Url, '올바른 웹사이트 URL을 입력해주세요.').max(64, 'URL은 최대 64자까지만 가능합니다.'),
+	url: Yup.string().matches(HTTPProtocol, 'http:// 또는 https:// 로 시작해야합니다.').matches(Url, '올바른 초대링크 URL을 입력해주세요.').max(64, 'URL은 최대 64자까지만 가능합니다.'),
+	git: Yup.string().matches(HTTPProtocol, 'http:// 또는 https:// 로 시작해야합니다.').matches(Url, '올바른 깃 URL을 입력해주세요.').max(64, 'URL은 최대 64자까지만 가능합니다.'),
+	discord: Yup.string().matches(Vanity, '디스코드 초대코드 형식을 지켜주세요.').min(2, '지원 디스코드는 최소 2자여야합니다.').max(32, '지원 디스코드는 최대 32자까지만 가능합니다.'),
+	category: Yup.array(Yup.string().oneOf(categories)).min(1, '최소 한 개의 카테고리를 선택해주세요.').unique('카테고리는 중복될 수 없습니다.').required('카테고리는 필수 항목입니다.'),
+	intro: Yup.string().min(2, '봇 소개는 최소 2자여야합니다.').max(60, '봇 소개는 최대 60자여야합니다.').required('봇 소개는 필수 항목입니다.'),
+	desc: Yup.string().min(100, '봇 설명은 최소 100자여야합니다.').max(1500, '봇 설명은 최대 1500자여야합니다.').required('봇 설명은 필수 항목입니다.'),
+	owners: Yup.array(Yup.string()).min(1, '최소 한 명의 소유자는 입력해주세요.').max(10, '소유자는 최대 10명까지만 가능합니다.').unique('소유자 아이디는 중복될 수 없습니다.').required('소유자는 필수 항목입니다.'),
+	_csrf: Yup.string().required()
+})
 export default Yup

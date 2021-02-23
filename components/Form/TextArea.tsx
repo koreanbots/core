5c596978b72a4052fa91c701d13f06b47dbac913
Junseo Park <wonderlandpark@outlook.kr>
2021-02-23 10:25:34 +0900

    feat: textarea size fixed

--- components/Form/TextArea.tsx
+++ components/Form/TextArea.tsx
@@ -1,7 +1,7 @@
 import { Field } from 'formik'
 
 const TextArea = ({ name, placeholder }:TextAreaProps):JSX.Element => {
-	return <Field as='textarea' name={name} className='border border-grey-light dark:border-transparent text-black dark:bg-very-black dark:text-white w-full rounded px-4 py-3 relative min-h-2 max-h-3 outline-none' placeholder={placeholder} />
+	return <Field as='textarea' name={name} className='border border-grey-light dark:border-transparent text-black dark:bg-very-black dark:text-white w-full rounded px-4 py-3 relative min-h-3 resize-none outline-none' placeholder={placeholder} />
 }
 
 interface TextAreaProps {

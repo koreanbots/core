8ea709bd676a06593be2daade8ae065569ab5d5a
Junseo Park <wonderlandpark@outlook.kr>
2021-02-25 13:17:29 +0900

    feat: fetching members

--- utils/DiscordBot.ts
+++ utils/DiscordBot.ts
@@ -3,7 +3,11 @@ import * as Discord from 'discord.js'
 const DiscordBot = new Discord.Client()
 
 const guildID = '653083797763522580'
-DiscordBot.on('ready', () => console.log('I\'m Ready'))
+DiscordBot.on('ready', async () => {
+	console.log('I\'m Ready')
+	await getMainGuild().members.fetch()
+	console.log(`Fetched ${getMainGuild().members.cache.size} Members`)
+})
 
 DiscordBot.login(process.env.DISCORD_TOKEN)
 

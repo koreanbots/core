905469c5931c3e330091fc42325ea1ddb7e7d1a4
Junseo Park <wonderlandpark@outlook.kr>
2021-02-25 22:54:43 +0900

    chore: changed api docs route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -57,7 +57,7 @@ const Navbar = (): JSX.Element => {
 								</Link>
 							</li>
 							<li className='flex items-center'>
-								<Link href='/api'>
+								<Link href='/developers/docs'>
 									<a className='lg:hover:text-gray-300 flex items-center px-3 py-4 w-full hover:text-gray-500 text-gray-700 text-sm font-semibold sm:w-auto lg:py-2 lg:text-gray-100'>
 										API
 									</a>
@@ -163,7 +163,7 @@ const Navbar = (): JSX.Element => {
 							<span className='px-2 font-medium'>소개</span>
 						</a>
 					</Link>
-					<Link href='/api'>
+					<Link href='/developers/docs'>
 						<a onClick={()=> setNavbarOpen(false)} className='flex items-center px-8 py-2 text-gray-100 hover:text-gray-300'>
 							<i className='fas fa-tools' />
 

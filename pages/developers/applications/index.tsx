205bc8150b199b467ba7c381daf22c93eb6638d2
Junseo Park <wonderlandpark@outlook.kr>
2021-02-27 13:48:37 +0900

    feat: grid responsive

--- pages/developers/applications/index.tsx
+++ pages/developers/applications/index.tsx
@@ -7,20 +7,19 @@ import { parseCookie } from '@utils/Tools'
 import { Bot, User } from '@types'
 
 const Application = dynamic(() => import('@components/Application'))
-const Container = dynamic(() => import('@components/Container'))
-const SEO = dynamic(() => import('@components/SEO'))
+const DeveloperLayout = dynamic(() => import('@components/DeveloperLayout'))
 
 const Applications: NextPage<ApplicationsProps> = ({ user }) => {
-	return <Container paddingTop>
-		<SEO title='개발자' description='한국 디스코드봇 리스트 API를 활용하여 봇에 다양한 기능을 추가해보세요.' />
+	return <DeveloperLayout enabled='applications'>
 		<h1 className='text-3xl font-bold'>나의 봇</h1>
 		<p className='text-gray-400'>한국 디스코드봇 리스트 API를 활용하여 봇에 다양한 기능을 추가해보세요.</p>
-		<div className='grid grid-cols-8 gap-4 mt-10'>
+		<div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4 mt-2'>
 			{
 				(user.bots as Bot[]).map(bot => <Application key={bot.id} id={bot.id} name={bot.name} type='bot' />)
 			}
 		</div>
-	</Container>
+	</DeveloperLayout>
+
 }
 
 interface ApplicationsProps {

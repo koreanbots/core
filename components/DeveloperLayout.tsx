163670a0a29c97c05797f98621079babb275fb1f
Junseo Park <wonderlandpark@outlook.kr>
2021-02-27 14:52:05 +0900

    chore: Developer Layout header color

--- components/DeveloperLayout.tsx
+++ components/DeveloperLayout.tsx
@@ -9,6 +9,7 @@ const DeveloperLayout = ({ children, enabled }:DeveloperLayout):JSX.Element => {
 	return <div className='flex h-screen'>
 		<SEO title='한디리 개발자' description='한국 디스코드봇 리스트 API를 활용하여 봇에 다양한 기능을 추가해보세요.' />
 		<div className='w-1/8 lg:hidden pt-20 px-2 h-screen text-center bg-little-white dark:bg-discord-black'>
+			<h2 className='font-black text-koreanbots-blue pb-4'><i className='fas fa-tools'/></h2>
 			<ul className='text-gray-300'>
 				<li className={`hover:text-white cursor-pointer py-2 px-4 mb-2 rounded-md ${enabled === 'applications' ? 'bg-discord-blurple text-white' : ''}`}>
 					<Link href='/developers/applications'><i className='fas fa-robot'/></Link>
@@ -19,7 +20,7 @@ const DeveloperLayout = ({ children, enabled }:DeveloperLayout):JSX.Element => {
 			</ul>
 		</div>
 		<div className={'lg:w-1/6 bg-little-white dark:bg-discord-black pt-20 px-6 hidden lg:block h-screen'} >
-			<h2 className='font-black text-lg pb-4'>KOREANBOTS DEVELOPERS</h2>
+			<h2 className='font-black text-2xl pb-4 text-koreanbots-blue'>DEVELOPERS</h2>
 			<ul className='text-base text-gray-300'>
 				<li className={`hover:text-white cursor-pointer py-2 px-4 rounded-md ${enabled === 'applications' ? 'bg-discord-blurple text-white' : ''}`}>
 					<Link href='/developers/applications'>나의 봇</Link>

9751727b5d075769425c365fdfca461edc395b90
Junseo Park <wonderlandpark@outlook.kr>
2021-02-27 11:02:23 +0900

    chore: transition duration decrease

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link'
 
 const Button = ({ type='button', className, children, href, onClick }: ButtonProps):JSX.Element => {
 	return href ? <Link href={href}>
-		<a className={`cursor-pointer rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
+		<a className={`cursor-pointer rounded-md px-4 py-2 m-1 transition duration-300 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
 			{ children }
 		</a>
-	</Link> : onClick ? <button type={type} onKeyDown={onClick} onClick={onClick} className={`cursor-pointer rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
+	</Link> : onClick ? <button type={type} onKeyDown={onClick} onClick={onClick} className={`cursor-pointer rounded-md px-4 py-2 m-0.5 transition duration-300 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
 		{ children }
-	</button> : <button type={type} className={`cursor-pointer rounded-md px-4 py-2 m-2 transition duration-500 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
+	</button> : <button type={type} className={`cursor-pointer rounded-md px-4 py-2 m-0.5 transition duration-300 ease select-none outline-none foucs:outline-none ${className ?? 'bg-discord-blurple hover:opacity-80 dark:bg-very-black dark:hover:bg-discord-dark-hover text-white'}`}>
 		{ children }
 	</button>
 }

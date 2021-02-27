eaf0842672b799edd0f8d131760b83c224445577
Junseo Park <wonderlandpark@outlook.kr>
2021-02-27 13:46:40 +0900

    feat: added whitesapce no wrap

--- components/Application.tsx
+++ components/Application.tsx
@@ -4,8 +4,8 @@ import DiscordAvatar from './DiscordAvatar'
 const Application = ({ type, id, name }:ApplicationProps):JSX.Element => {
 	return <Link href={`/developers/applications/${type+'s'}/${id}`}>
 		<div className='relative py-4 px-2 bg-little-white dark:bg-discord-black text-center transform hover:-translate-y-1 transition duration-100 ease-in cursor-pointer rounded-lg'>
-			<DiscordAvatar userID={id} className='w-full rounded-lg px-2' />
-			<h2 className='text-xl font-bold pt-2'>{name.slice(0, 7)}{name.length > 7 && '...'}</h2>
+			<DiscordAvatar userID={id} className='w-full rounded-xl px-2' />
+			<h2 className='text-xl font-medium pt-2 whitespace-nowrap truncate'>{name}</h2>
 		</div>
 	</Link>
 }

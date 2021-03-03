67ed95f7b606fced13028d5e7dd7584c420371f0
wonderlandpark <wonderbotsupport@naver.com>
2021-03-03 17:25:49 +0900

    chore: changed grid column

--- components/ResponsiveGrid.tsx
+++ components/ResponsiveGrid.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
 const ResponsiveGrid = ({ children }:{ children: ReactNode }):JSX.Element => {
-	return <div className='grid gap-x-4 grid-rows-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 mt-10 -mb-10'>
+	return <div className='grid gap-x-4 grid-rows-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-10 -mb-10'>
 		{children}
 	</div>
 }

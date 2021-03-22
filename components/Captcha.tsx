3c7fd5939a5872af15e2462e5cf4ad109fbe7cb5
wonderlandpark <wonderbotsupport@naver.com>
2021-03-22 12:49:27 +0900

    feat: added captcha component

---
+++ components/Captcha.tsx
@@ -0,0 +1,12 @@
+import HCaptcha from '@hcaptcha/react-hcaptcha'
+
+const Captcha = ({ dark, onVerify }:CaptchaProps):JSX.Element => {
+	return <HCaptcha sitekey='43e556b4-cc90-494f-b100-378b906bb736' theme={dark ? 'dark' : 'light'} onVerify={onVerify}/>
+}
+
+interface CaptchaProps {
+  dark: boolean
+  onVerify(token: string, eKey?: string): void
+}
+
+export default Captcha

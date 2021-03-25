7a013d864b78e8ea949778e3c42d386cffbc11af
wonderlandpark <wonderbotsupport@naver.com>
2021-03-25 12:58:47 +0900

    chore: improved checkbox design

--- components/Form/CheckBox.tsx
+++ components/Form/CheckBox.tsx
@@ -1,7 +1,7 @@
 import { Field } from 'formik'
 
 const CheckBox = ({ name, ...props }: CheckBoxProps): JSX.Element => {
-	return <Field type='checkbox' name={name} className='mr-1 w-4 h-4 rounded' {...props} />
+	return <Field type='checkbox' name={name} className='form-checkbox text-koreanbots-blue bg-gray-300 h-4 w-4 rounded' {...props} />
 }
 
 interface CheckBoxProps {

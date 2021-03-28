81fb011edbbea01c5aa2ec498ece8d83b4f7b17f
wonderlandpark <wonderbotsupport@naver.com>
2021-03-28 13:00:41 +0900

    chore: not debuging knex at development also

--- utils/Knex.ts
+++ utils/Knex.ts
@@ -8,6 +8,5 @@ export default knex({
 		password: process.env.MYSQL_PASSWORD,
 		database: process.env.MYSQL_DATABASE || 'discordbots',
 		charset: 'utf8mb4',
-	},
-	debug: process.env.NODE_ENV === 'development',
+	}
 })

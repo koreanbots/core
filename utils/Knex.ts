376eb45443722c2ca2e64ed367409a4536ea0c88
wonderlandpark <wonderlandpark@outlook.kr>
2021-04-13 13:56:55 +0900

    chore: defaulting mysql password

--- utils/Knex.ts
+++ utils/Knex.ts
@@ -5,7 +5,7 @@ export default knex({
 	connection: {
 		host: process.env.MYSQL_HOST || 'localhost',
 		user: process.env.MYSQL_USER || 'root',
-		password: process.env.MYSQL_PASSWORD,
+		password: process.env.MYSQL_PASSWORD || 'test',
 		database: process.env.MYSQL_DATABASE || 'discordbots',
 		charset: 'utf8mb4',
 	}

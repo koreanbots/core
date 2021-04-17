7056c1ffa91f310fa7d20274a2d2f41251e311b6
Junseo Park <wonderbotsupport@naver.com>
2021-04-17 11:18:22 +0900

    Update components/Loader.tsx
    Co-authored-by: zero734kr <zero734kr@gmail.com>

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ const Loader = ({ text, visible = true }: LoaderProps): JSX.Element => {
 		<div
 			className={`${
 				visible ? '' : 'hidden '
-			}w-full h-full fixed block top-0 left-0 bg-gray-500 bg-opacity-75 z-50 dark:text-black`}
+			} w-full h-full fixed block top-0 left-0 bg-gray-500 bg-opacity-75 z-50 dark:text-black`}
 		>
 			<h1 className='relative top-1/2 block mx-auto my-0 text-center text-2xl font-semibold opacity-100'>
 				{text}

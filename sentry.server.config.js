e18f05aba61850bdaaa4256f89161ae82dbe4e0e
wonderlandpark <wonderlandpark@outlook.kr>
2021-05-07 22:22:29 +0900

    chore: debugging

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -8,7 +8,8 @@ const SENTRY_DSN = process.env.SENTRY_DSN
 
 Sentry.init({
 	dsn: SENTRY_DSN,
-	release: undefined
+	debug: true,
+	enabled: process.env.NODE_DEV === 'production'
 	// Note: if you want to override the automatic release value, do not set a
 	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
 	// that it will also get attached to your source maps

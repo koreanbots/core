3242b6e292594dae6f484d9f11caa639efbfe8fa
wonderlandpark <wonderlandpark@outlook.kr>
2021-05-07 22:45:51 +0900

    chore: skiping sentry auto release

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// const { withSentryConfig } = require('@sentry/nextjs')
-
 const { withSentryConfig } = require('@sentry/nextjs')
 const VERSION = require('./package.json').version
 
 module.exports = withSentryConfig({
 	env: {
-		NEXT_PUBLIC_RELEASE_VERSION: VERSION
+		NEXT_PUBLIC_RELEASE_VERSION: VERSION,
+		SENTRY_SKIP_AUTO_RELEASE: true
 	},
 	future: {
 		webpack5: true,

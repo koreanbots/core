540d596c250ecca551c100c5b4f3368a8c8aed8d
wonderlandpark <wonderlandpark@outlook.kr>
2021-05-09 10:34:45 +0900

    fix: sentry disabled

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -9,7 +9,7 @@ const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN
 Sentry.init({
 	dsn: SENTRY_DSN,
 	debug: true,
-	enabled: process.env.NODE_DEV === 'production'
+	enabled: process.env.NODE_ENV === 'production'
 	// Note: if you want to override the automatic release value, do not set a
 	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
 	// that it will also get attached to your source maps

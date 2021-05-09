f4899357cae1b46694564a95f68c2c2a59adcf2f
wonderlandpark <wonderlandpark@outlook.kr>
2021-05-09 11:52:26 +0900

    fix: home not displayed at dev portal
    fix: https://github.com/koreanbots/v2-testing/issues/84

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -151,10 +151,14 @@ const Navbar = ({ token }:{ token: string }): JSX.Element => {
 				}`}
 			>
 				<nav className='mt-20'>
-					<Link href='/developers'>
+					<Link href={dev ? '/' : '/developers'}>
 						<a onClick={()=> setNavbarOpen(false)} className='flex items-center px-8 py-2 text-gray-100 hover:text-gray-300'>
-							<i className='fas fa-tools' />
-							<span className='px-2 font-medium'>개발자</span>
+							{
+								dev ? <i className='fas fa-home' /> : <i className='fas fa-tools' />
+							}
+							<span className='px-2 font-medium'>	
+								{dev ? '홈' : '개발자'}
+							</span>
 						</a>
 					</Link>
 					<Link href='/discord'>
@@ -165,26 +169,13 @@ const Navbar = ({ token }:{ token: string }): JSX.Element => {
 					</Link>
 					<Link href='/about'>
 						<a onClick={()=> setNavbarOpen(false)} className='flex items-center px-8 py-2 text-gray-100 hover:text-gray-300'>
-							<svg
-								className='w-5 h-5'
-								viewBox='0 0 24 24'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'
-							>
-								<path
-									d='M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17'
-									stroke='currentColor'
-									strokeWidth='2'
-									strokeLinecap='round'
-									strokeLinejoin='round'
-								/>
-							</svg>
-							<span onClick={() => setNavbarOpen(!navbarOpen)} className='px-2 font-medium'>소개</span>
+							<i className='fas fa-layer-group' />
+							<span className='px-2 font-medium'>소개</span>
 						</a>
 					</Link>
 					<Link href='/addbot'>
 						<a onClick={()=> setNavbarOpen(false)} className='flex items-center px-8 py-2 text-gray-100 hover:text-gray-300'>
-							<i className='fas fa-plus'></i>
+							<i className='fas fa-plus' />
 							<span className='px-2 font-medium'>봇 추가하기</span>
 						</a>
 					</Link>

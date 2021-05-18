2071eba398fa4a0bb380d1ce36fb95c5214dd998
wonderlandpark <wonderlandpark@outlook.kr>
2021-05-18 10:55:50 +0900

    feat(iOS): added pwa splash screen

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -76,7 +76,16 @@ class MyDocument extends Document {
 					<link rel='apple-touch-icon' sizes='180x180' href='/static/apple-icon-180x180.png' />
 					<link rel='apple-touch-icon' sizes='256x256' href='/static/apple-icon-256x256.png' />
 					<link rel='apple-touch-icon' sizes='512x512' href='/static/apple-icon-512x512.png' />
-
+					<link rel='apple-touch-startup-image' href='/static/iphone5_splash.png' media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)' />
+					<link rel='apple-touch-startup-image' href='/static/iphone6_splash.png' media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)' />
+					<link rel='apple-touch-startup-image' href='/static/iphoneplus_splash.png' media='(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)' />
+					<link rel='apple-touch-startup-image' href='/static/iphonex_splash.png' media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)' />
+					<link rel='apple-touch-startup-image' href='/static/iphonexr_splash.png' media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)' />
+					<link rel='apple-touch-startup-image' href='/static/iphonexsmax_splash.png' media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)' />
+					<link rel='apple-touch-startup-image' href='/static/ipad_splash.png' media='(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)' />
+					<link rel='apple-touch-startup-image' href='/static/ipadpro1_splash.png' media='(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)' />
+					<link rel='apple-touch-startup-image' href='/static/ipadpro3_splash.png' media='(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)' />
+					<link rel='apple-touch-startup-image' href='/static/ipadpro2_splash.png' media='(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)' />
 
 					{/* Android */}
 					<link rel='icon' type='image/png' sizes='192x192'  href='/static/android-icon-192x192.png' />

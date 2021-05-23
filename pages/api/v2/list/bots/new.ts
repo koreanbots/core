24fa10ca6f232d59f38e2ee85f445fbad20bf774
wonderlandpark <wonderlandpark@outlook.kr>
2021-05-23 10:38:51 +0900

    feat: added bot lists

---
+++ pages/api/v2/list/bots/new.ts
@@ -0,0 +1,13 @@
+import { get } from '@utils/Query'
+import RequestHandler from '@utils/RequestHandler'
+import ResponseWrapper from '@utils/ResponseWrapper'
+
+import { BotList } from '@types'
+import Yup from '@utils/Yup'
+
+const NewList = RequestHandler().get(async (_req, res) => {
+	const result = await get.list.votes.load(1)
+	return ResponseWrapper<BotList>(res, { code: 200, data: result })
+})
+
+export default NewList

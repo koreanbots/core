4800fa840986a35f3c01d611167b386b7d04ae87
wonderlandpark <wonderlandpark@outlook.kr>
2021-05-23 10:39:30 +0900

    style: removed unused variable

--- pages/api/v2/list/bots/new.ts
+++ pages/api/v2/list/bots/new.ts
@@ -3,7 +3,6 @@ import RequestHandler from '@utils/RequestHandler'
 import ResponseWrapper from '@utils/ResponseWrapper'
 
 import { BotList } from '@types'
-import Yup from '@utils/Yup'
 
 const NewList = RequestHandler().get(async (_req, res) => {
 	const result = await get.list.votes.load(1)

5820b70afdb4461868962f7dc48e8dfb15c7aa4f
wonderlandpark <wonderlandpark@outlook.kr>
2021-06-02 12:53:00 +0900

    fix: adsense

--- components/Advertisement.tsx
+++ components/Advertisement.tsx
@@ -1,37 +1,19 @@
-import { useEffect } from 'react'
-import Logger from '@utils/Logger'
+import AdSense from 'react-adsense'
 
 const Advertisement: React.FC<AdvertisementProps> = ({ size = 'short' }) => {
-	useEffect(() => {
-		if (process.env.NODE_ENV === 'production') {
-			window.adsbygoogle = window.adsbygoogle || []
-			try {
-				window.adsbygoogle.push({})
-				Logger.debug('Ad Pushed')
-			}
-			catch {
-				Logger.debug('Ad Push Fail')
-			}
-		}
-		
-	}, [])
-
 	return <div className='py-5'>
 		<div
 			className={`z-0 mx-auto w-full text-center text-white ${
-				process.env.NODE_ENV === 'production' ? '' : 'py-12 bg-gray-700'
+				true ? '' : 'py-12 bg-gray-700'
 			}`}
 			style={size === 'short' ? { height: '90px' } : { height: '330px' }}
 		>
-			{process.env.NODE_ENV === 'production' ? (
-				<ins
-					className='adsbygoogle'
-					style={{ display: 'block', width: '100%' }}
-					data-ad-client='ca-pub-4856582423981759'
-					data-ad-slot='3250141451'
-					data-ad-format={size === 'short' ? 'horizontal' : 'vertical'}
-					data-full-width-responsive='true'
-					data-adtest='on'
+			{true ? (
+				<AdSense.Google
+					style={{ display: 'inline-block', width: '100%', height: size === 'short' ? '90px' : '330px'}}
+					client='ca-pub-4856582423981759'
+					slot='3250141451'
+					format=''
 				/>
 			) : (
 				'Advertisement'

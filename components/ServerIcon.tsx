99e8bd4a162c3ea999555f099dc5cf8ce790a4d3
wonderlandpark <wonderlandpark@outlook.kr>
2021-08-15 13:07:21 +0900

    feat(ServerIcon): added ServerIcon component

---
+++ components/ServerIcon.tsx
@@ -0,0 +1,35 @@
+import { SyntheticEvent } from 'react'
+import dynamic from 'next/dynamic'
+import { DiscordEnpoints, KoreanbotsEndPoints } from '@utils/Constants'
+
+const Image = dynamic(() => import('@components/Image'))
+
+const DiscordAvatar: React.FC<DiscordAvatarProps> = ({ id, size, className, alt, hash }) => {
+	return <Image
+		className={className}
+		alt={alt}
+		src={hash ? DiscordEnpoints.CDN.guild(id, hash, { format: 'webp', size: size ?? 256 }) : KoreanbotsEndPoints.CDN.icon(id, { format: 'webp', size: size ?? 256})}
+		fallbackSrc={hash ? DiscordEnpoints.CDN.guild(id, hash, { format: 'png', size: size ?? 256 }) : KoreanbotsEndPoints.CDN.icon(id, { format: 'png', size: size ?? 256})}
+	/>
+	
+}
+
+interface DiscordAvatarProps {
+	alt?: string
+	id: string
+	hash?: string
+	fromDiscord?: boolean
+	className?: string
+	size? : 128 | 256 | 512
+}
+
+interface ImageEvent extends Event {
+	target: ImageTarget
+}
+
+interface ImageTarget extends EventTarget {
+	src: string
+	onerror: (event: SyntheticEvent<HTMLImageElement, ImageEvent>) => void
+}
+
+export default DiscordAvatar

1198d88845890374d32b98bbc85786a2da2d927d
wonderlandpark <wonderlandpark@outlook.kr>
2021-08-16 12:57:11 +0900

    chore(Hero): support serverlist

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,13 @@
 import dynamic from 'next/dynamic'
 import { NextSeo } from 'next-seo'
 
-import { botCategories, botCategoryIcon } from '@utils/Constants'
+import { botCategories, botCategoryIcon, serverCategories } from '@utils/Constants'
 
 const Container = dynamic(()=> import('@components/Container'))
 const Tag = dynamic(()=> import('@components/Tag'))
 const Search = dynamic(()=> import('@components/Search'))
 
-const Hero:React.FC<HeroProps> = ({ header, description }) => {
+const Hero:React.FC<HeroProps> = ({ type, header, description }) => {
 	return <>
 		<NextSeo title={header} description={description} openGraph={{
 			title: header,
@@ -16,18 +16,18 @@ const Hero:React.FC<HeroProps> = ({ header, description }) => {
 		<div className='dark:bg-discord-black bg-discord-blurple text-gray-100 md:p-0 mb-8'>
 			<Container className='pt-24 pb-16 md:pb-20' ignoreColor>
 				<h1 className='hidden md:block text-left text-3xl font-bold'>
-					{ header && `${header} - `}한국 디스코드봇 리스트
+					{ header && `${header} - `}한국 디스코드 리스트
 				</h1>
 				<h1 className='md:hidden text-center text-3xl font-semibold'>
-					{ header && <span className='text-4xl'>{header}<br/></span>}한국 디스코드봇 리스트
+					{ header && <span className='text-4xl'>{header}<br/></span>}한국 디스코드 리스트
 				</h1>
-				<p className='text-center sm:text-left text-xl font-base mt-2'>{description || '다양한 국내 디스코드봇을 한곳에서 확인하세요!'}</p>
+				<p className='text-center sm:text-left text-xl font-base mt-2'>{description || `다양한 국내 디스코드 ${type === 'bots' ? '봇을' : '서버를'} 한 곳에서 확인하세요!`}</p>
 				<Search />
 				<div className='flex flex-wrap mt-5'>
 					<Tag key='list' text={<>
 						<i className='fas fa-heart text-red-600'/> 하트 랭킹
 					</>} dark bigger href='/list/votes' />
-					{ botCategories.slice(0, 4).map(t=> <Tag key={t} text={<>
+					{ (type === 'bots' ? botCategories : serverCategories).slice(0, 4).map(t=> <Tag key={t} text={<>
 						<i className={botCategoryIcon[t]} /> {t}
 					</>} dark bigger href={`/categories/${t}`} />) }
 					<Tag key='tag' text={<>
@@ -40,6 +40,7 @@ const Hero:React.FC<HeroProps> = ({ header, description }) => {
 }
 
 interface HeroProps {
+	type: 'bots' | 'servers'
   header?: string
   description?: string
 }

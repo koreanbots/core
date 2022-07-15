581356703e4e08a56ecf74371957d8af2e1b90a9
chul0721 <chul070721@naver.com>
2022-07-15 16:44:12 +0900

    fix: resolve add button bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -217,7 +217,7 @@ const Navbar: React.FC<NavbarProps> = ({ token }) => {
 					</Link>
 					<a
 						onClick={()=> {
-							setMobileAddDropdownOpen(false)
+							setMobileAddDropdownOpen(!mobileAddDropdownOpen)
 						}}
 						className='flex items-center px-8 py-2 text-gray-100'
 					>

be988f8afa24d30c624eef73d8a899b378eb223c
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2022-08-14 23:34:11 +0900

    chore: do not include submit page

--- pages/api/v2/management/bots/submits/[id]/[date]/deny.ts
+++ pages/api/v2/management/bots/submits/[id]/[date]/deny.ts
@@ -20,7 +20,7 @@ const DenyBotSubmit = RequestHandler()
 		const embed = new MessageEmbed().setTitle('거부').setColor('RED').setDescription(`[${submit.id}/${submit.date}](${KoreanbotsEndPoints.URL.submittedBot(submit.id, submit.date)})`).setTimestamp()
 		if(req.body.reviewer || req.body.reason) embed.addField('📃 정보', `${req.body.reason ? `사유: ${BotSubmissionDenyReasonPresetsName[req.body.reason] || req.body.reason}\n`: ''}${req.body.reviewer ? `심사자: ${req.body.reviewer}` : ''}`)
 		await getBotReviewLogChannel().send(embed)
-		const openEmbed = new MessageEmbed().setTitle('거부').setColor('RED').setDescription(`[${submit.id}/${submit.date}](${KoreanbotsEndPoints.URL.submittedBot(submit.id, submit.date)})`).setTimestamp()
+		const openEmbed = new MessageEmbed().setTitle('거부').setColor('RED').setDescription(`${submit.id}/${submit.date}`).setTimestamp()
 		if(req.body.reason) openEmbed.addField('📃 정보', `${req.body.reason ? `사유: ${BotSubmissionDenyReasonPresetsName[req.body.reason] || req.body.reason}\n`: ''}`)
 		await getOpenBotReviewLogChannel().send(embed)
 		tracer.trace('botSubmits.deny', span => {

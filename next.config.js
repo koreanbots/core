9c5a3160a822bc0a33c767ad80d7785479da61c5
Eunwoo Choi <choi@eunwoo.dev>
2023-01-20 13:32:43 +0900

    deps: update next-pwa and dd-trace@2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { withSentryConfig } = require('@sentry/nextjs')
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+	disable: process.env.NODE_ENV !== 'production',
+	register: false
+})
 const VERSION = require('./package.json').version
 
 const NextConfig = {
@@ -9,10 +12,6 @@ const NextConfig = {
 			config.resolve.fallback.fs = false
 		}
 		return config
-	},
-	pwa: {
-		disable: process.env.NODE_ENV !== 'production',
-		register: false
 	},
 	env: {
 		NEXT_PUBLIC_RELEASE_VERSION: VERSION,

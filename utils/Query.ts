ea3060c85f1b45ba99924f8e8e6ebeffc3affe8c
SKINMAKER <me@skinmaker.dev>
2023-02-01 19:32:24 +0800

    fix: check bot strikes properly (#536)

--- utils/Query.ts
+++ utils/Query.ts
@@ -407,7 +407,7 @@ async function submitBot(id: string, data: AddBotSubmit):Promise<1|2|3|4|5|Submi
 	const submits = await knex('submitted').select(['id']).where({ state: 0 }).andWhere('owners', 'LIKE', `%${id}%`)
 	if(submits.length > 1) return 1
 	const botId = data.id
-	const strikes = await get.botSubmitStrikes(id)
+	const strikes = await get.botSubmitStrikes(botId)
 	if(strikes >= 3) return 5
 	const date =  Math.round(+new Date()/1000)
 	const sameID = await knex('submitted').select(['id']).where({ id: botId, state: 0 })

86ab70e1ac8cfdbf8065d61d1a44b472cb9362be
Eunwoo Choi <choi@eunwoo.dev>
2023-04-06 12:37:46 +0900

    chore: rename webhook to webhook_url

--- sql/webhook.sql
+++ sql/webhook.sql
@@ -3,7 +3,8 @@ use discordbots;
 
 -- bots TABLE
 ALTER TABLE `bots` ADD COLUMN webhook_status INT NOT NULL DEFAULT '1';
+ALTER TABLE `bots` RENAME COLUMN webhook TO webhook_url;
 
 -- servers TABLE
-ALTER TABLE `servers` ADD COLUMN webhook TEXT DEFAULT NULL;
+ALTER TABLE `servers` ADD COLUMN webhook_url TEXT DEFAULT NULL;
 ALTER TABLE `servers` ADD COLUMN webhook_status INT NOT NULL DEFAULT '1';

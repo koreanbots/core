14e94dbb486a97c9b9e4fdea72d1b81cb471300d
Seoyeon <55011525+rihayoru@users.noreply.github.com>
2023-06-18 23:42:11 +0900

    feat: add shuffleArray function to Tools.ts

--- utils/Tools.ts
+++ utils/Tools.ts
@@ -215,4 +215,13 @@ export function convertMetrixToGraph(data: MetrixData[], keyname?: string) {
 	}))
 }
 
+export function shuffleArray<T>(array: T[]): T[] {
+	const shuffled = [...array]
+	for (let i = shuffled.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+	}
+	return shuffled
+}
+
 export * from './ShowdownExtensions'

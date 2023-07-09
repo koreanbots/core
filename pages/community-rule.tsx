0af48f1eb637ea5fab1f6a8d825cc358d83af732
SKINMAKER <me@skinmaker.dev>
2023-07-09 13:10:23 +0900

    feat: add community rule (#618)
    * feat: add community rule
    
    * fix: rename guideline to community rule

---
+++ pages/community-rule.tsx
@@ -0,0 +1,37 @@
+import { GetStaticProps, NextPage } from 'next'
+import dynamic from 'next/dynamic'
+
+import { SpecialEndPoints } from '@utils/Constants'
+
+
+const Docs = dynamic(()=> import('@components/Docs'))
+const Markdown = dynamic(() => import('@components/Markdown'))
+
+
+const CommunityRule: NextPage<CommunityRuleProps> = ({ content }) => {
+	return (
+		<Docs
+			header='커뮤니티 규칙'
+			description='한국 디스코드 리스트 커뮤니티 규칙입니다.'
+		>
+			<Markdown text={content} />
+		</Docs>
+	)
+}
+
+interface CommunityRuleProps {
+	content: string
+}
+
+export const getStaticProps: GetStaticProps<CommunityRuleProps> = async () => {
+	const res = await fetch(SpecialEndPoints.Github.Content('koreanbots', 'terms', 'community-rule.md'))
+	const json = await res.json()
+	return {
+		props: {
+			content: Buffer.from(json.content, 'base64').toString('utf-8')
+		}
+	}
+}
+
+export default CommunityRule
+

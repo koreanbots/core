2161e45ee36b8af2c980c72248b37195382d0664
Eunwoo Choi <choi@eunwoo.dev>
2025-01-20 11:30:04 +0900

    feat: revert adsense code

--- components/Advertisement.tsx
+++ components/Advertisement.tsx
@@ -1,7 +1,31 @@
 import AdSense from 'react-adsense'
 
 const Advertisement: React.FC<AdvertisementProps> = ({ size = 'short' }) => {
-	return null
+	return (
+		<div className='py-5'>
+			<div
+				className={`z-0 mx-auto w-full text-center text-white ${
+					process.env.NODE_ENV === 'production' ? '' : 'bg-gray-700 py-12'
+				}`}
+				style={size === 'short' ? { height: '90px' } : { height: '330px' }}
+			>
+				{process.env.NODE_ENV === 'production' ? (
+					<AdSense.Google
+						style={{
+							display: 'inline-block',
+							width: '100%',
+							height: size === 'short' ? '90px' : '330px',
+						}}
+						client='ca-pub-2090578791829639'
+						slot='3250141451'
+						format=''
+					/>
+				) : (
+					'Advertisement'
+				)}
+			</div>
+		</div>
+	)
 }
 
 declare global {

ad4fa7a10138f92994183c3bc7c9c38a2d355e45
wonderlandpark <wonderlandpark@outlook.kr>
2025-03-22 23:44:47 +0900

    type: typing issue

--- pages/bots/[id]/edit.tsx
+++ pages/bots/[id]/edit.tsx
@@ -619,6 +619,7 @@ export const getServerSideProps = async (ctx: Context) => {
 	const user = await get.Authorization(parsed?.token)
 	const bot = await get.bot.load(ctx.query.id)
 	const spec = await get.botSpec(ctx.query.id, user)
+	const spec = await get.botSpec(ctx.query.id, user || '')
 	return {
 		props: {
 			bot: { ...bot, banner: spec.banner, bg: spec.bg },

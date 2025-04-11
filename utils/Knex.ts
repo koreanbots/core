6e37411764bea3cd703eb6131788a5224655d3a4
SKINMAKER <skinmaker1345@gmail.com>
2025-04-11 20:17:56 +0900

    chore: add MYSQL_PORT in env variable

--- utils/Knex.ts
+++ utils/Knex.ts
@@ -3,6 +3,7 @@ import knex from 'knex'
 export default knex({
 	client: 'mysql',
 	connection: {
+		port: Number(process.env.MYSQL_PORT) || 3306,
 		host: process.env.MYSQL_HOST || 'localhost',
 		user: process.env.MYSQL_USER || 'root',
 		password: process.env.MYSQL_PASSWORD || 'test',

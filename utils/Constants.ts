c9df8d01e3c59b5b68c9f5f7d6d2c2b0788b11f1
skinmaker1345 <skinmaker1345@gmail.com>
2025-06-14 21:34:58 +0900

    chore: specify enforcement description

--- utils/Constants.ts
+++ utils/Constants.ts
@@ -122,7 +122,7 @@ export const botCategoryDescription = {
 
 export const botEnforcements = {
 	'서버 참여가 강제되는 명령어가 있습니다': 'JOIN_PARTIALLY_ENFORCED',
-	'서버 참여 없이는 봇을 사용할 수 없습니다': 'JOIN_ENFORCED',
+	'서버 참여 없이는 봇의 핵심 기능을 사용할 수 없습니다': 'JOIN_ENFORCED',
 }
 
 export const botCategoryIcon = {
